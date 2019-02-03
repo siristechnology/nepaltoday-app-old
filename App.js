26e1afe09d72880987a1a2d4f927dab766654b3a
Suraj Shrestha <syuraj@gmail.com>
2019-02-02 23:06:57 -0500

    a native-base theme added from https://reactnativeseed.com/\?utm_source\=startreact\&utm_medium\=website\&utm_campaign\=startreact

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import { StyleProvider } from "native-base";
 import AppContainer from './src/frame/app-container';
+import getTheme from "./src/native-base-theme/components";
+import variables from "./src/native-base-theme/variables/platform";
 
 type Props = {};
 export default class App extends Component<Props> {
@@ -13,7 +16,9 @@ export default class App extends Component<Props> {
 	render () {
 		//TODO: Need a splash screen while app is loading
 		return (
-			<AppContainer />
+			<StyleProvider style={getTheme(variables)}>
+				<AppContainer />
+			</StyleProvider>
 		);
 	}
 }
