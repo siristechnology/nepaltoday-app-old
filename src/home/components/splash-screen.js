7ea02935a0e9a18658226bb72497e8bc5b56925e
Suresh Shrestha <shrestha2lt8@gmail.com>
2019-02-22 22:10:11 +0545

    fixed the error

--- src/home/components/splash-screen.js
+++ src/home/components/splash-screen.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-class SplashScreen extends React.Component {
+export default class SplashScreen extends React.Component {
   render() {
+	const styles = StyleSheet.create({
+		container: {
+		  flex: 1,
+		  justifyContent: 'center',
+		  alignItems: 'center',
+		  backgroundColor: '#F5FCFF',
+		},
+		welcome: {
+		  fontSize: 20,
+		  textAlign: 'center',
+		  margin: 10,
+		},
+		instructions: {
+		  textAlign: 'center',
+		  color: '#333333',
+		  marginBottom: 5,
+		},
+	  });
+	  
     const viewStyles = [styles.container, { backgroundColor: 'skyblue' }];
     const heading = {
       color: 'white',
