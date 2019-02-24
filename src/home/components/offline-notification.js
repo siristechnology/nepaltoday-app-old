9ccacf05b995535a05b459726a514c18d1d3e78a
Suresh Shrestha <shrestha2lt8@gmail.com>
2019-02-24 17:33:43 +0545

    rollbacked optimization

--- src/home/components/offline-notification.js
+++ src/home/components/offline-notification.js
@@ -1,52 +1,55 @@
-import { PureComponent } from 'react';
+import React, { PureComponent } from 'react';
 import { View, Text, NetInfo, Dimensions, StyleSheet } from 'react-native';
 
 const { width } = Dimensions.get('window');
 
-class OfflineNotice extends PureComponent {
-	constructor (props) {
-		super(props);
-		this.state = {
-			isConnected: true
-		};
-	}
-
-	componentDidMount () {
-		NetInfo.isConnected.addEventListener('connectionChange', (isConnected) => this.setState({ isConnected }));
-	}
-
-	componentWillUnmount () {
-		NetInfo.isConnected.removeEventListener('connectionChange', (isConnected) => this.setState({ isConnected }));
-	}
-
-	render () {
-		if (!this.state.isConnected) {
-			return <MiniOfflineSign />;
-		}
-		return null;
-	}
+function MiniOfflineSign() {
+  return (
+	<View style={styles.offlineContainer}>
+		<Text style={styles.offlineText}>कृपया इन्टरनेट जाँच गर्नुहोस्</Text>
+	</View>
+  );
 }
 
-class MiniOfflineSign extends PureComponent {
-	render () {
-		return (
-			<View style={styles.offlineContainer}>
-				<Text style={styles.offlineText}>कृपया इन्टरनेट जाँच गर्नुहोस्</Text>
-			</View>
-		);
-	}
+class OfflineNotice extends PureComponent {
+  state = {
+    isConnected: true
+  };
+
+  componentDidMount() {
+    NetInfo.isConnected.addEventListener('connectionChange', this.handleConnectivityChange);
+  }
+
+  componentWillUnmount() {
+    NetInfo.isConnected.removeEventListener('connectionChange', this.handleConnectivityChange);
+  }
+
+  handleConnectivityChange = isConnected => {
+    if (isConnected) {
+      this.setState({ isConnected });
+    } else {
+      this.setState({ isConnected });
+    }
+  };
+
+  render() {
+    if (!this.state.isConnected) {
+      return <MiniOfflineSign />;
+    }
+    return null;
+  }
 }
 
 const styles = StyleSheet.create({
-	offlineContainer: {
-		backgroundColor: '#b52424',
-		height: 30,
-		flexDirection: 'row',
-		justifyContent: 'center',
-		alignItems: 'center',
-		width,
-	},
-	offlineText: { color: 'white' }
+  offlineContainer: {
+    backgroundColor: '#b52424',
+	height: 30,
+	flexDirection: 'row',
+    justifyContent: 'center',
+	alignItems: 'center',
+    width,
+  },
+  offlineText: { color: 'white' }
 });
 
 export default OfflineNotice;
