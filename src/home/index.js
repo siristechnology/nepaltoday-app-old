7467b5a2b5aa1226fe40e7abfa0991c27d48db2b
Suraj Shrestha <syuraj@gmail.com>
2019-02-24 19:31:14 -0500

    Use AppState to determine if app comes to front from background and refresh (#14)

--- src/home/index.js
+++ src/home/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FlatList, RefreshControl } from 'react-native';
+import { FlatList, RefreshControl, AppState } from 'react-native';
 import { QueryRenderer, graphql } from 'react-relay';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
@@ -15,16 +15,30 @@ class Home extends React.PureComponent {
 	constructor (props) {
 		super(props);
 		this.state = {
-			isUpdated: false
+			isUpdated: false,
+			appState: AppState.currentState
 		};
 	}
 
 	componentDidMount () {
 		Analytics.trackEvent('Home page load');
+		AppState.addEventListener('change', this._handleAppStateChange.bind(this));
 	}
 
 	componentDidUpdate () {
 		Analytics.trackEvent('Home page refresh');
+	}
+
+	componentWillUnmount () {
+		AppState.removeEventListener('change', this._handleAppStateChange.bind(this));
+	}
+
+	_handleAppStateChange (nextAppState) {
+		if (this.state.appState.match(/inactive|background/) && nextAppState === 'active') {
+			console.log('App has come to the foreground!')
+			this.handleRefresh()
+		}
+		this.setState({ appState: nextAppState });
 	}
 
 	handleRefresh () {
