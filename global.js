504c32608de1c19f4926219b29102247b784be42
Suraj Shrestha <syuraj@gmail.com>
2019-04-18 22:52:54 -0400

    Fix server url after moving from azure to heroku docker

--- global.js
+++ global.js
@@ -1,3 +1,3 @@
 export default {
-	nepalTodayServer: 'https://nepaltodayapi.azurewebsites.net/api/nepalTodayApi?code=myHqlIZBK3Ns3ynvlkgMaTZpgXD64ge8y9Ry9lR3IbZgIK1WpQVg9Q=='
+	nepalTodayServer: 'https://nepaltoday-api.herokuapp.com/graphql'
 }
