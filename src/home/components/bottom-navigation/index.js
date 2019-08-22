4badbf3848ee74ef007faba97bc0f3066444628b
shrijan-siris <shrijan.siristechnology@gmail.com>
2019-08-22 09:37:00 +0545

    nep-20/bottom-navigation: basic app navigation added

--- src/home/components/bottom-navigation/index.js
+++ src/home/components/bottom-navigation/index.js
@@ -1,53 +1,26 @@
 import React, { Component } from 'react'
-import { Icon } from 'react-native-elements'
 import styled from 'styled-components/native'
 import colors from '../../../config/colors'
+import { Footer, FooterTab, Button, Text, Icon } from 'native-base'
 export default class ButtonNavigation extends Component {
 	render () {
 		return (
-			<ButtomNavigation>
-				<TouchableOpacityWrapper>
-					<StyledIcon name="home" type="material-community" />
-					<StyledText>Home</StyledText>
-				</TouchableOpacityWrapper>
-				<TouchableOpacityWrapper>
-					<StyledIcon name="newspaper" type="material-community" />
-					<StyledText>Headlines</StyledText>
-				</TouchableOpacityWrapper>
-				<TouchableOpacityWrapper>
-					<StyledIcon name="twitter" type="material-community" />
-					<StyledText>Twitter</StyledText>
-				</TouchableOpacityWrapper>
-			</ButtomNavigation>
+			<Footer>
+				<FooterTab>
+					<Button vertical>
+						<Icon ios="ios-menu" name="home" />
+						<Text>Home</Text>
+					</Button>
+					<Button vertical>
+						<Icon name="newspaper" type="FontAwesome5" />
+						<Text>Headlines</Text>
+					</Button>
+					<Button vertical active>
+						<Icon active ios="twitter" android="twitter" type="FontAwesome" />
+						<Text>Twitter</Text>
+					</Button>
+				</FooterTab>
+			</Footer>
 		)
 	}
 }
-
-const ButtomNavigation = styled.View`
-	width: 100%;
-	height: 45;
-	background: ${colors.GREY};
-	display: flex;
-	flex-direction: row;
-	align-items: center;
-	justify-content: space-between;
-`
-
-const TouchableOpacityWrapper = styled.TouchableOpacity`
-	flex: 0.2;
-	align-items: center;
-	justify-content: center;
-`
-
-const StyledIcon = styled(Icon)`
-	background-color: transparent;
-	opacity: 0.8;
-	font-size: 24;
-`
-const StyledText = styled.Text`
-	background-color: transparent;
-	padding-top: 4;
-	font-size: 12;
-	font-family: roboto-regular;
-	color: #616161;
-`
