c90c59f8d4d87c469f94224874a502fa10233b42
shrijan-siris <shrijan.siristechnology@gmail.com>
2019-08-22 12:22:40 +0545

    NEP-21/swipe-navigation: swipe navigation package added

--- src/home/components/article-card.js
+++ src/home/components/article-card.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import { Image, StyleSheet } from 'react-native';
-import { Card, CardItem, Thumbnail, Text, Left, Body, View } from 'native-base';
-import Analytics from 'appcenter-analytics'
-import actionCreators from '../ducks/actions.js';
+import React from 'react'
 import moment from 'moment'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import Analytics from 'appcenter-analytics'
+import { Image, StyleSheet } from 'react-native'
+import { Card, CardItem, Thumbnail, Text, Left, Body, View } from 'native-base'
+
+import actionCreators from '../ducks/actions.js'
 
 class ArticleCard extends React.PureComponent {
 	handleArticleCardPressed () {
-		this.props.actions.startToOpenArticle(this.props.article);
+		this.props.actions.startToOpenArticle(this.props.article)
 
-		Analytics.trackEvent('Article link click');
-		const { navigation } = this.props;
-		navigation.navigate('Article', { article: this.props.article });
+		Analytics.trackEvent('Article link click')
+		const { navigation } = this.props
+		navigation.navigate('Article', { article: this.props.article })
 	}
 
 	render () {
-		const { article } = this.props;
+		const { article } = this.props
 
 		moment.updateLocale('en', {
 			relativeTime: {
@@ -29,36 +30,73 @@ class ArticleCard extends React.PureComponent {
 				d: '1 दिन',
 				dd: '%d दिन'
 			}
-		});
-		const relativeTime = moment(Number(article.publishedDate || article.modifiedDate)).startOf('hour').fromNow()
+		})
+		const relativeTime = moment(
+			Number(article.publishedDate || article.modifiedDate)
+		)
+			.startOf('hour')
+			.fromNow()
 
 		return (
 			<Card transparent style={{ flex: 1, borderBottomWidth: 0.4 }}>
-				<CardItem button onPress={this.handleArticleCardPressed.bind(this)}
-					style={{ paddingTop: 12, paddingLeft: 10, paddingRight: 10, paddingBottom: 4 }} activeOpacity={1}>
+				<CardItem
+					button
+					onPress={this.handleArticleCardPressed.bind(this)}
+					style={{
+						paddingTop: 12,
+						paddingLeft: 10,
+						paddingRight: 10,
+						paddingBottom: 4
+					}}
+					activeOpacity={1}
+				>
 					<Body>
 						<View style={styles.imageContainer}>
-							<Image source={{ uri: article.imageLink }} resizeMethod='scale' style={styles.image} />
+							<Image
+								source={{ uri: article.imageLink }}
+								resizeMethod="scale"
+								style={styles.image}
+							/>
 						</View>
-						<View style={{ flex: 1, flexDirection: 'row', alignItems: 'baseline', marginTop: 6, marginBottom: 12 }}>
-							<Thumbnail source={{ uri: article.source.logoLink }} style={{ width: 20, height: 20, borderRadius: 20 / 2, marginRight: 4 }} />
+						<View
+							style={{
+								flex: 1,
+								flexDirection: 'row',
+								alignItems: 'baseline',
+								marginTop: 6,
+								marginBottom: 12
+							}}
+						>
+							<Thumbnail
+								source={{ uri: article.source.logoLink }}
+								style={{
+									width: 20,
+									height: 20,
+									borderRadius: 20 / 2,
+									marginRight: 4
+								}}
+							/>
 							<Text style={styles.newsSource}>{article.source.name}</Text>
 						</View>
 						<View style={{ flex: 1, justifyContent: 'flex-start' }}>
-							<Text style={{ fontSize: 19, includeFontPadding: true }}>{article.title}</Text>
+							<Text style={{ fontSize: 19, includeFontPadding: true }}>
+								{article.title}
+							</Text>
 							<Text>{article.shortDescription}</Text>
 						</View>
 					</Body>
 				</CardItem>
 				<CardItem style={{ paddingTop: 0, paddingBottom: 4, marginBottom: 0 }}>
-					<Left >
-						<View style={{ flex: 1, flexDirection: 'row', alignItems: 'baseline' }}>
-							<Text note >{relativeTime}</Text>
+					<Left>
+						<View
+							style={{ flex: 1, flexDirection: 'row', alignItems: 'baseline' }}
+						>
+							<Text note>{relativeTime}</Text>
 						</View>
 					</Left>
 				</CardItem>
 			</Card>
-		);
+		)
 	}
 }
 
@@ -84,15 +122,17 @@ const styles = StyleSheet.create({
 		fontSize: 16,
 		paddingRight: 10
 	}
-});
+})
 
 function mapStateToProps () {
-	return {
-	};
+	return {}
 }
 
 function mapDispatchToProps (dispatch) {
 	return { actions: bindActionCreators(actionCreators, dispatch) }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(ArticleCard)
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(ArticleCard)
