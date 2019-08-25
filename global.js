b3d74bfa00cfa35bc514df87dbf4a2d08e8783d0
shrijan-siris <shrijan.siristechnology@gmail.com>
2019-08-25 15:08:34 +0545

    NEP-23-twitter-feed-screen: getting articles from localhost with genymotion proxy server

--- global.js
+++ global.js
@@ -1,3 +1,4 @@
 export default {
-	nepalTodayServer: 'https://nepaltoday-api.herokuapp.com/graphql'
+	// nepalTodayServer: 'https://nepaltoday-api.herokuapp.com/graphql'
+	nepalTodayServer: 'http://10.0.3.2:5000/graphql'
 }
