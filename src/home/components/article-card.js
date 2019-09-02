6d47a0104d2a702879f523386460a6391208a662
Suraj Shrestha <syuraj@gmail.com>
2019-09-02 16:34:27 -0400

    Nep 23 active state (#13)
    * NEP-20/button-navigation-bar: setup file is added and package.json is modified [can be removed if working without it]
    
    * NEP-20/button-navigation-bar: group imports and ladder rule applied
    
    * NEP-20/button-navigation-bar: basic bottom navigation is added
    
    * nep-20/bottom-navigation: basic app navigation added
    
    * nep-20/bottom-navigation: finally choosen the native one
    
    * nep-20/bottom-navigation: setup.js file deleted
    
    * nep-20/bottom-navigation: simple test added for bottom navigation
    
    * NEP-21/swipe-navigation: swipe navigation package added
    
    * NEP-21/swipe-navigation: basic navigation added to home and twitter handler
    
    * NEP-21/twitter-collection: basic card for twitter handler is designed
    
    * NEP-21 adding queryrenderer in twitter screen
    
    * NEP-23-twitter-feed-screen: twitter handle screen is designed
    
    * NEP-23-twitter-feed-screen: getting articles from localhost with genymotion proxy server
    
    * NEP-23-twitter-feed-screen: retrieve tweets and displayed in twitter screen
    
    * nep-23-active-state: api url changes along with active navigation added
    
    * nep-23-active-state: grandle and build properties added
    
    * NEP-23 Reverting gradle build to 3.3.2 to fix release apk build failure
    
    * nep-23-active-state: category added
    
    * nep-23-active-state: build grandle after realase apk
    
    * nep-23-active-state: alert removed
    
    * nep-23-active-state: test - jest mockup added

--- src/home/components/article-card.js
+++ src/home/components/article-card.js
@@ -1,98 +1,142 @@
-import React from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import { Image, StyleSheet } from 'react-native';
-import { Card, CardItem, Thumbnail, Text, Left, Body, View } from 'native-base';
-import Analytics from 'appcenter-analytics'
-import actionCreators from '../ducks/actions.js';
+import React from 'react'
 import moment from 'moment'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import Analytics from 'appcenter-analytics'
+import { Image, StyleSheet } from 'react-native'
+import { Card, CardItem, Thumbnail, Text, Left, Body, View } from 'native-base'
+
+import actionCreators from '../ducks/actions.js'
 
 class ArticleCard extends React.PureComponent {
-	handleArticleCardPressed () {
-		this.props.actions.startToOpenArticle(this.props.article);
+  handleArticleCardPressed() {
+    this.props.actions.startToOpenArticle(this.props.article)
 
-		Analytics.trackEvent('Article link click');
-		const { navigation } = this.props;
-		navigation.navigate('Article', { article: this.props.article });
-	}
+    Analytics.trackEvent('Article link click')
+    const { navigation } = this.props
+    navigation.navigate('Article', { article: this.props.article })
+  }
 
-	render () {
-		const { article } = this.props;
+  render() {
+    const { article } = this.props
 
-		moment.updateLocale('en', {
-			relativeTime: {
-				past: '%s अघि',
-				m: '1 मिनेट',
-				mm: '%d मिनेट',
-				h: '1 घण्टा',
-				hh: '%d घण्टा',
-				d: '1 दिन',
-				dd: '%d दिन'
-			}
-		});
-		const relativeTime = moment(Number(article.publishedDate || article.modifiedDate)).startOf('hour').fromNow()
+    moment.updateLocale('en', {
+      relativeTime: {
+        past: '%s अघि',
+        m: '1 मिनेट',
+        mm: '%d मिनेट',
+        h: '1 घण्टा',
+        hh: '%d घण्टा',
+        d: '1 दिन',
+        dd: '%d दिन'
+      }
+    })
+    const relativeTime = moment(
+      Number(article.publishedDate || article.modifiedDate)
+    )
+      .startOf('hour')
+      .fromNow()
 
-		return (
-			<Card transparent style={{ flex: 1, borderBottomWidth: 0.4 }}>
-				<CardItem button onPress={this.handleArticleCardPressed.bind(this)}
-					style={{ paddingTop: 12, paddingLeft: 10, paddingRight: 10, paddingBottom: 4 }} activeOpacity={1}>
-					<Body>
-						<View style={styles.imageContainer}>
-							<Image source={{ uri: article.imageLink }} resizeMethod='scale' style={styles.image} />
-						</View>
-						<View style={{ flex: 1, flexDirection: 'row', alignItems: 'baseline', marginTop: 6, marginBottom: 12 }}>
-							<Thumbnail source={{ uri: article.source.logoLink }} style={{ width: 20, height: 20, borderRadius: 20 / 2, marginRight: 4 }} />
-							<Text style={styles.newsSource}>{article.source.name}</Text>
-						</View>
-						<View style={{ flex: 1, justifyContent: 'flex-start' }}>
-							<Text style={{ fontSize: 19, includeFontPadding: true }}>{article.title}</Text>
-							<Text>{article.shortDescription}</Text>
-						</View>
-					</Body>
-				</CardItem>
-				<CardItem style={{ paddingTop: 0, paddingBottom: 4, marginBottom: 0 }}>
-					<Left >
-						<View style={{ flex: 1, flexDirection: 'row', alignItems: 'baseline' }}>
-							<Text note >{relativeTime}</Text>
-						</View>
-					</Left>
-				</CardItem>
-			</Card>
-		);
-	}
+    return (
+      <Card transparent style={{ flex: 1, borderBottomWidth: 0.4 }}>
+        <CardItem
+          button
+          onPress={this.handleArticleCardPressed.bind(this)}
+          style={{
+            paddingTop: 12,
+            paddingLeft: 10,
+            paddingRight: 10,
+            paddingBottom: 4
+          }}
+          activeOpacity={1}
+        >
+          <Body>
+            <View style={styles.imageContainer}>
+              <Image
+                source={{ uri: article.imageLink }}
+                resizeMethod="scale"
+                style={styles.image}
+              />
+            </View>
+            <View
+              style={{
+                flex: 1,
+                flexDirection: 'row',
+                alignItems: 'baseline',
+                marginTop: 6,
+                marginBottom: 12
+              }}
+            >
+              <Thumbnail
+                source={{ uri: article.source.logoLink }}
+                style={{
+                  width: 20,
+                  height: 20,
+                  borderRadius: 20 / 2,
+                  marginRight: 4
+                }}
+              />
+              <Text style={styles.newsSource}>{article.source.name}</Text>
+            </View>
+            <View style={{ flex: 1, justifyContent: 'flex-start' }}>
+              <Text style={{ fontSize: 19, includeFontPadding: true }}>
+                {article.title}
+              </Text>
+              <Text>{article.shortDescription}</Text>
+            </View>
+          </Body>
+        </CardItem>
+        <CardItem style={{ paddingTop: 0, paddingBottom: 4, marginBottom: 0 }}>
+          <Left>
+            <View
+              style={{
+                flex: 1,
+                flexDirection: 'row',
+                alignItems: 'baseline'
+              }}
+            >
+              <Text note>{relativeTime}</Text>
+            </View>
+          </Left>
+        </CardItem>
+      </Card>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-	imageContainer: {
-		flex: 1,
-		flexDirection: 'row',
-		alignItems: 'stretch',
-		backgroundColor: 'rgba(27,31,35,.05)',
-		elevation: 1,
-		margin: 2,
-		borderRadius: 8
-	},
-	image: {
-		flex: 1,
-		height: 200,
-		top: 0,
-		borderRadius: 8,
-		borderWidth: 0.5
-	},
-	newsSource: {
-		color: '#737373',
-		fontSize: 16,
-		paddingRight: 10
-	}
-});
+  imageContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    backgroundColor: 'rgba(27,31,35,.05)',
+    elevation: 1,
+    margin: 2,
+    borderRadius: 8
+  },
+  image: {
+    flex: 1,
+    height: 200,
+    top: 0,
+    borderRadius: 8,
+    borderWidth: 0.5
+  },
+  newsSource: {
+    color: '#737373',
+    fontSize: 16,
+    paddingRight: 10
+  }
+})
 
-function mapStateToProps () {
-	return {
-	};
+function mapStateToProps() {
+  return {}
 }
 
-function mapDispatchToProps (dispatch) {
-	return { actions: bindActionCreators(actionCreators, dispatch) }
+function mapDispatchToProps(dispatch) {
+  return { actions: bindActionCreators(actionCreators, dispatch) }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(ArticleCard)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ArticleCard)
---
+++ src/twitter/TwitterCard.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Text, ListItem, Body, Left, Thumbnail, Right, View } from 'native-base'
+import styled from 'styled-components/native'
+import { MutedText } from '../styled'
+
+class TwitterCard extends React.PureComponent {
+	render () {
+		const { tweet } = this.props
+		return (
+			<ListItem avatar>
+				<Left>
+					<StyledThumbnail
+						source={{
+							uri: 'https://api.adorable.io/avatars/157/abott@adorable.png'
+						}}
+					/>
+				</Left>
+				<Body>
+					<NameWrapper>
+						<Text style={{ paddingRight: 8 }}>{tweet.twitterHandle.name}</Text>
+						<MutedText>{tweet.twitterHandle.handle}</MutedText>
+					</NameWrapper>
+					<Text note>{tweet.text}</Text>
+				</Body>
+			</ListItem>
+		)
+	}
+}
+export default TwitterCard
+
+const StyledThumbnail = styled(Thumbnail)`
+	width: 40px;
+	height: 40px;
+	margin-top: 0;
+`
+const NameWrapper = styled(View)`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+`
