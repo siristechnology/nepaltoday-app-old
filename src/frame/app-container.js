6a112debbb02da36249a53fd1616854d92cd5106
shrijan-siris <shrijan.siristechnology@gmail.com>
2019-09-04 12:40:48 +0545

    nep-23-active-state: linting updated

--- src/frame/app-container.js
+++ src/frame/app-container.js
@@ -3,12 +3,14 @@ import { createStackNavigator, createAppContainer } from 'react-navigation'
 import Home from '../home/index.js'
 import TwitterScreen from '../twitter/index'
 import ArticleScreen from '../article/index.js'
+import BottomNavigation from '../home/components/bottom-navigation/index'
 
 const AppNavigator = createStackNavigator(
 	{
 		Home: { screen: Home },
 		Article: { screen: ArticleScreen },
-		Twitter: { screen: TwitterScreen }
+		Twitter: { screen: TwitterScreen },
+		BottomNavigation: { screen: BottomNavigation }
 	},
 	{
 		headerMode: 'none',
--- src/home/components/bottom-navigation/index.js
+++ src/home/components/bottom-navigation/index.js
@@ -1,41 +1,37 @@
-import React, { Component } from "react";
-import { withNavigation } from "react-navigation";
-import { Footer, FooterTab, Button, Text, Icon } from "native-base";
+import React, { Component } from 'react'
+import { withNavigation } from 'react-navigation'
+import { Footer, FooterTab, Button, Text, Icon } from 'native-base'
 
 class BottomNavigation extends Component {
   getIfActive = param => {
-    return this.props.navigation.state.routeName === param;
-  };
+    return this.props.navigation.state.routeName === param
+  }
   render() {
-    const { navigate } = this.props.navigation;
+    const { navigate } = this.props.navigation
     return (
       <Footer>
         <FooterTab>
           <Button
             vertical
             active
-            onPress={() => navigate("Home")}
-            active={this.getIfActive("Home")}
+            onPress={() => navigate('Home')}
+            active={this.getIfActive('Home')}
           >
             <Icon ios="ios-menu" name="home" />
             <Text>Home</Text>
           </Button>
-          {/* <Button vertical>
-						<Icon name="newspaper" type="FontAwesome5" />
-						<Text>Headlines</Text>
-					</Button> */}
           <Button
             vertical
-            onPress={() => navigate("Twitter")}
-            active={this.getIfActive("Twitter")}
+            onPress={() => navigate('Twitter')}
+            active={this.getIfActive('Twitter')}
           >
             <Icon active ios="twitter" android="twitter" type="FontAwesome" />
             <Text>Twitter</Text>
           </Button>
         </FooterTab>
       </Footer>
-    );
+    )
   }
 }
 
-export default withNavigation(BottomNavigation);
+export default withNavigation(BottomNavigation)
