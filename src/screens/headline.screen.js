518f45db30a7806dd41c96d9f655e39c7fd0268d
shrijan-siris <shrijan.siristechnology@gmail.com>
2019-09-05 20:20:42 +0545

    NEP-40-categories: basic tab nav is added content is still need to be displayed

--- src/screens/headline.screen.js
+++ src/screens/headline.screen.js
@@ -1,11 +1,64 @@
 import React from 'react'
-import { Spinner } from 'native-base'
+import { Spinner, Header, Tabs, Tab, Container } from 'native-base'
 import { FlatList } from 'react-native'
 import { QueryRenderer, graphql } from 'react-relay'
 
 import environment from '../environment'
 import AppLayout from '../frame/AppLayout'
 import ArticleCard from '../home/components/article-card'
+
+const renderQuery = ({ error, props }) => {
+	const renderTab = () => {
+		const tabs = [
+			...new Set(props.getArticles.map(article => article.category))
+		]
+		if (tabs) {
+			tabs.map(tab => {
+				const dataArr = props.getArticles.filter(a => a.category === tab)
+				if (dataArr.length <= 0) {
+					return <Text>Not available</Text>
+				}
+
+				return (
+					<Tabs>
+						<Tab heading={`${tab}`}>
+							<FlatList
+								data={dataArr}
+								keyExtractor={item => item._id}
+								renderItem={({ item }) => {
+									return (
+										<ArticleCard
+											article={item}
+											key={item._id}
+											actions={() => {}}
+											navigation={{}}
+										/>
+									)
+								}}
+							/>
+						</Tab>
+					</Tabs>
+				)
+			})
+		}
+	}
+	if (error) {
+		return <Text>{error.message}</Text>
+	} else if (props) {
+		return (
+			<AppLayout>
+				{props && props.getArticles.length > 0 ? (
+					<Container>
+						<Header hasTabs />
+						{renderTab()}
+					</Container>
+				) : null}
+			</AppLayout>
+		)
+	}
+	return <Spinner color="blue" />
+}
+
 class HeadlineScreen extends React.PureComponent {
 	render () {
 		return (
@@ -31,36 +84,7 @@ class HeadlineScreen extends React.PureComponent {
             }
           }
         `}
-				render={({ error, props }) => {
-					console.log('props here in headline page', props)
-					if (!props) {
-						return <Spinner color="blue" />
-					} else if (error) {
-						console.log('error:' + JSON.stringify(error))
-					}
-
-					return (
-						<AppLayout>
-							{props && props.getArticles.length > 0 && (
-								<FlatList
-									data={props.getArticles}
-									keyExtractor={item => item._id}
-									extraData={this.state}
-									renderItem={({ item }) => {
-										return (
-											<ArticleCard
-												article={item}
-												key={item._id}
-												actions={this.props.actions}
-												navigation={this.props.navigation}
-											/>
-										)
-									}}
-								/>
-							)}
-						</AppLayout>
-					)
-				}}
+				render={renderQuery}
 			/>
 		)
 	}
