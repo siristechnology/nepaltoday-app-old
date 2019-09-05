522f52e8e84c9aa1093f0a0e43662336098deb29
shrijan-siris <shrijan.siristechnology@gmail.com>
2019-09-05 19:16:19 +0545

    NEP-40-categories: spiner added

--- src/screens/headline.screen.js
+++ src/screens/headline.screen.js
@@ -1,64 +1,68 @@
 import React from 'react'
-import { FlatList } from 'native-base'
+import { FlatList, Spinner } from 'native-base'
 import { QueryRenderer, graphql } from 'react-relay'
 
 import environment from '../environment'
 import AppLayout from '../frame/AppLayout'
 import ArticleCard from '../home/components/article-card'
-
-export const HeadlineScreen = ({ actions, navigation }) => {
-	return (
-		<QueryRenderer
-			environment={environment}
-			query={graphql`
-        query headlineQuery {
-          getArticles {
-            _id
-            title
-            shortDescription
-            content
-            link
-            imageLink
-            publishedDate
-            modifiedDate
-            category
-            source {
+class HeadlineScreen extends React.PureComponent {
+	render () {
+		return (
+			<QueryRenderer
+				environment={environment}
+				query={graphql`
+          query headlineQuery {
+            getArticles {
               _id
-              name
-              logoLink
+              title
+              shortDescription
+              content
+              link
+              imageLink
+              publishedDate
+              modifiedDate
+              category
+              source {
+                _id
+                name
+                logoLink
+              }
             }
           }
-        }
-      `}
-			render={({ error, props }) => {
-				console.log('props here in headline page', props)
-				if (!props) {
-					console.log('data not found')
-				} else if (error) {
-					console.log('error:' + JSON.stringify(error))
-				}
+        `}
+				render={({ error, props }) => {
+					console.log('props here in headline page', props)
+					if (!props) {
+						return <Spinner color="blue" />
+					} else if (error) {
+						console.log('error:' + JSON.stringify(error))
+					}
 
-				return (
-					<AppLayout>
-						{props && props.getArticles && (
-							<FlatList
-								data={props.getArticles}
-								keyExtractor={item => item._id}
-								renderItem={({ item }) => {
-									return (
-										<ArticleCard
-											article={item}
-											key={item._id}
-											actions={actions}
-											navigation={navigation}
-										/>
-									)
-								}}
-							/>
-						)}
-					</AppLayout>
-				)
-			}}
-		/>
-	)
+					return (
+						<AppLayout>
+							{props && props.getArticles.length > 0 && (
+								<FlatList
+									data={props.getArticles}
+									keyExtractor={item => item._id}
+									extraData={this.state}
+									renderItem={({ item }) => {
+										return (
+											<ArticleCard
+												article={item}
+												key={item._id}
+												actions={this.props.actions}
+												navigation={this.props.navigation}
+											/>
+										)
+									}}
+								/>
+							)}
+						</AppLayout>
+					)
+				}}
+			/>
+		)
+	}
 }
+
+export default HeadlineScreen
