fc9263f04a948dbb1f428f251adfa4c0c29f9a86
shrijan-siris <shrijan.siristechnology@gmail.com>
2019-09-05 10:32:24 +0545

    NEP-42-news-details: bottom navigation removed from app-contaner

--- src/frame/app-container.js
+++ src/frame/app-container.js
@@ -3,7 +3,6 @@ import { createStackNavigator, createAppContainer } from 'react-navigation'
 import Home from '../home/index.js'
 import TwitterScreen from '../twitter/index'
 import ArticleScreen from '../article/index.js'
-import BottomNavigation from '../home/components/bottom-navigation/index'
 import ArticleDetailScreen from '../home/components/article-detail.screen.js'
 
 const AppNavigator = createStackNavigator(
@@ -11,8 +10,7 @@ const AppNavigator = createStackNavigator(
 		Home: { screen: Home },
 		Article: { screen: ArticleScreen },
 		ArticleDetail: { screen: ArticleDetailScreen },
-		Twitter: { screen: TwitterScreen },
-		BottomNavigation: { screen: BottomNavigation }
+		Twitter: { screen: TwitterScreen }
 	},
 	{
 		headerMode: 'none',
