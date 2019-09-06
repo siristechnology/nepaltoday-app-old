528237c103fecb4ed329476264737141182bebae
shrijan-siris <shrijan.siristechnology@gmail.com>
2019-09-06 09:49:26 +0545

    NEP-40-categories: navigation completed

--- src/screens/HeadlineScreen.js
+++ src/screens/HeadlineScreen.js
@@ -17,65 +17,64 @@ import AppLayout from '../frame/AppLayout'
 import ArticleCard from '../home/components/article-card'
 import { getLocalName } from '../helper/text'
 
-const renderQuery = ({ error, props }) => {
-	if (!props) {
-		return <Spinner />
-	} else if (error) {
-		console.log('error:' + JSON.stringify(error))
-	}
-	const renderTab = () => {
-		const tabs = [
-			...new Set(props.getArticles.map(article => article.category))
-		]
-
-		if (tabs) {
-			return tabs.map((tab, idx) => {
-				const dataArr = props.getArticles.filter(a => a.category === tab)
-				if (dataArr.length <= 0) {
-					return <Text>Not available</Text>
-				}
-
-				const tabName = getLocalName(tab)
-
-				return (
-					<Tab heading={`${tabName}`} key={idx}>
-						<FlatList
-							data={dataArr}
-							keyExtractor={item => item._id}
-							renderItem={({ item }) => {
-								return (
-									<ArticleCard
-										article={item}
-										key={item._id}
-										actions={() => {}}
-										navigation={{}}
-									/>
-								)
-							}}
-						/>
-					</Tab>
-				)
-			})
-		}
-	}
-	if (error) {
-		return <Text>{error.message}</Text>
-	} else if (props) {
-		return (
-			<AppLayout>
-				{props && props.getArticles.length > 0 ? (
-					<Container>
-						<Header hasTabs style={styles.header} />
-						<Tabs renderTabBar={() => <ScrollableTab />}>{renderTab()}</Tabs>
-					</Container>
-				) : null}
-			</AppLayout>
-		)
-	}
-	return <Spinner color="blue" />
-}
-
 class HeadlineScreen extends React.PureComponent {
+	renderQuery = ({ error, props }) => {
+		if (!props) {
+			return <Spinner />
+		} else if (error) {
+			console.log('error:' + JSON.stringify(error))
+		}
+		const renderTab = () => {
+			const tabs = [
+				...new Set(props.getArticles.map(article => article.category))
+			]
+
+			if (tabs) {
+				return tabs.map((tab, idx) => {
+					const dataArr = props.getArticles.filter(a => a.category === tab)
+					if (dataArr.length <= 0) {
+						return <Text>Not available</Text>
+					}
+
+					const tabName = getLocalName(tab)
+
+					return (
+						<Tab heading={`${tabName}`} key={idx}>
+							<FlatList
+								data={dataArr}
+								keyExtractor={item => item._id}
+								renderItem={({ item }) => {
+									return (
+										<ArticleCard
+											article={item}
+											key={item._id}
+											actions={() => {}}
+											navigation={this.props.navigation}
+										/>
+									)
+								}}
+							/>
+						</Tab>
+					)
+				})
+			}
+		}
+		if (error) {
+			return <Text>{error.message}</Text>
+		} else if (props) {
+			return (
+				<AppLayout>
+					{props && props.getArticles.length > 0 ? (
+						<Container>
+							<Header hasTabs style={styles.header} />
+							<Tabs renderTabBar={() => <ScrollableTab />}>{renderTab()}</Tabs>
+						</Container>
+					) : null}
+				</AppLayout>
+			)
+		}
+		return <Spinner color="blue" />
+	}
 	render() {
 		return (
 			<QueryRenderer
@@ -100,7 +99,7 @@ class HeadlineScreen extends React.PureComponent {
 						}
 					}
 				`}
-				render={renderQuery}
+				render={this.renderQuery}
 			/>
 		)
 	}
