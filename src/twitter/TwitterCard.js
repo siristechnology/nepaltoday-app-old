d8b73ee8d66a1168e9a85b540760c2386fd6a246
shrijan-siris <shrijan.siristechnology@gmail.com>
2019-09-16 08:18:37 +0545

    error-boundary: twitter screen profile image updated

--- src/twitter/TwitterCard.js
+++ src/twitter/TwitterCard.js
@@ -1,26 +1,29 @@
 import React from 'react'
-import { Text, ListItem, Body, Left, Thumbnail, View } from 'native-base'
+import { Text, ListItem, Body, Thumbnail, View } from 'native-base'
 import styled from 'styled-components/native'
 import { MutedText } from '../styled'
+import { getRelativeTime } from '../helper/time'
 
 class TwitterCard extends React.PureComponent {
 	render() {
 		const { tweet } = this.props
+		const relativeTime = getRelativeTime(tweet.createdAt || new Date())
 		return (
 			<ListItem avatar>
-				<Left>
-					<StyledThumbnail
-						source={{
-							uri: 'https://api.adorable.io/avatars/157/abott@adorable.png'
-						}}
-					/>
-				</Left>
+				<StyledThumbnail
+					source={{
+						uri:
+							tweet.profileImage ||
+							'https://api.adorable.io/avatars/157/abott@adorable.png'
+					}}
+				/>
 				<Body>
 					<NameWrapper>
-						<Text style={{ paddingRight: 8 }}>{tweet.twitterHandle.name}</Text>
+						<Name>{tweet.twitterHandle.name}</Name>
 						<MutedText>{tweet.twitterHandle.handle}</MutedText>
+						<MutedText style={{ marginLeft: 8 }}>{relativeTime}</MutedText>
 					</NameWrapper>
-					<Text note>{tweet.text}</Text>
+					<Tweet>{tweet.text}</Tweet>
 				</Body>
 			</ListItem>
 		)
@@ -28,13 +31,21 @@ class TwitterCard extends React.PureComponent {
 }
 export default TwitterCard
 
+const Name = styled(Text)`
+	padding-right: 8px;
+	font-weight: 800;
+`
+
 const StyledThumbnail = styled(Thumbnail)`
 	width: 40px;
 	height: 40px;
-	margin-top: 0;
+	margin-top: -20;
 `
 const NameWrapper = styled(View)`
 	display: flex;
 	flex-direction: row;
 	align-items: center;
+`
+const Tweet = styled(Text)`
+	font-size: 14;
 `
--- src/twitter/__generated__/twitterQuery.graphql.js
+++ src/twitter/__generated__/twitterQuery.graphql.js
@@ -1,6 +1,6 @@
 /**
  * @flow
- * @relayHash d22baffbe28141f8cddf875b3ff53457
+ * @relayHash 6432e80a1341f1e84b9d95791b72531f
  */
 
 /* eslint-disable */
@@ -14,6 +14,10 @@ export type twitterQueryResponse = {|
   +getTweets: $ReadOnlyArray<?{|
     +_id: string,
     +text: ?string,
+    +name: ?string,
+    +profileImage: ?string,
+    +description: ?string,
+    +createdAt: ?string,
     +twitterHandle: ?{|
       +_id: string,
       +name: ?string,
@@ -34,6 +38,10 @@ query twitterQuery {
   getTweets {
     _id
     text
+    name
+    profileImage
+    description
+    createdAt
     twitterHandle {
       _id
       name
@@ -52,7 +60,14 @@ var v0 = {
   "args": null,
   "storageKey": null
 },
-v1 = [
+v1 = {
+  "kind": "ScalarField",
+  "alias": null,
+  "name": "name",
+  "args": null,
+  "storageKey": null
+},
+v2 = [
   {
     "kind": "LinkedField",
     "alias": null,
@@ -70,6 +85,28 @@ v1 = [
         "args": null,
         "storageKey": null
       },
+      (v1/*: any*/),
+      {
+        "kind": "ScalarField",
+        "alias": null,
+        "name": "profileImage",
+        "args": null,
+        "storageKey": null
+      },
+      {
+        "kind": "ScalarField",
+        "alias": null,
+        "name": "description",
+        "args": null,
+        "storageKey": null
+      },
+      {
+        "kind": "ScalarField",
+        "alias": null,
+        "name": "createdAt",
+        "args": null,
+        "storageKey": null
+      },
       {
         "kind": "LinkedField",
         "alias": null,
@@ -80,13 +117,7 @@ v1 = [
         "plural": false,
         "selections": [
           (v0/*: any*/),
-          {
-            "kind": "ScalarField",
-            "alias": null,
-            "name": "name",
-            "args": null,
-            "storageKey": null
-          },
+          (v1/*: any*/),
           {
             "kind": "ScalarField",
             "alias": null,
@@ -114,23 +145,23 @@ return {
     "type": "Query",
     "metadata": null,
     "argumentDefinitions": [],
-    "selections": (v1/*: any*/)
+    "selections": (v2/*: any*/)
   },
   "operation": {
     "kind": "Operation",
     "name": "twitterQuery",
     "argumentDefinitions": [],
-    "selections": (v1/*: any*/)
+    "selections": (v2/*: any*/)
   },
   "params": {
     "operationKind": "query",
     "name": "twitterQuery",
     "id": null,
-    "text": "query twitterQuery {\n  getTweets {\n    _id\n    text\n    twitterHandle {\n      _id\n      name\n      handle\n      category\n    }\n  }\n}\n",
+    "text": "query twitterQuery {\n  getTweets {\n    _id\n    text\n    name\n    profileImage\n    description\n    createdAt\n    twitterHandle {\n      _id\n      name\n      handle\n      category\n    }\n  }\n}\n",
     "metadata": {}
   }
 };
 })();
 // prettier-ignore
-(node/*: any*/).hash = '986834ef7725826f12e2341bc3afd2e9';
+(node/*: any*/).hash = 'ec328975b8c748937c4687aae23b54c8';
 module.exports = node;
