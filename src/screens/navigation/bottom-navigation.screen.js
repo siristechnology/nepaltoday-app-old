cd37f65f6c17f016c6dbbb44a1663f838ca8bdb0
shrijan-siris <shrijan.siristechnology@gmail.com>
2019-09-24 14:39:52 +0545

    hotfix/remove-bottombar-text: text from bottom navigation removed

--- src/screens/navigation/bottom-navigation.screen.js
+++ src/screens/navigation/bottom-navigation.screen.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createBottomTabNavigator } from 'react-navigation'
-import { Footer, FooterTab, Button, Icon, Text as NBText } from 'native-base'
+import { Footer, FooterTab, Button, Icon } from 'native-base'
 
 import HomeScreen from '../home-screen/home.screen'
 import TwitterScreen from '../twitter-screen/twitter.screen'
@@ -10,7 +10,7 @@ export const BottomTabScreen = createBottomTabNavigator(
 	{
 		Home: { screen: HomeScreen },
 		Headline: { screen: HeadlineScreen },
-		Twitter: { screen: TwitterScreen }
+		Twitter: { screen: TwitterScreen },
 	},
 	{
 		tabBarPosition: 'bottom',
@@ -22,36 +22,32 @@ export const BottomTabScreen = createBottomTabNavigator(
 						<Button
 							vertical
 							active={props.navigation.state.index === 0}
-							onPress={() => props.navigation.navigate('Home')}
-						>
+							onPress={() => props.navigation.navigate('Home')}>
 							<Icon ios="ios-menu" name="home" />
-							<NBText>Home</NBText>
 						</Button>
 						<Button
 							vertical
 							active={props.navigation.state.index === 1}
 							onPress={() =>
 								props.navigation.navigate('Headline')
-							}
-						>
+							}>
 							<Icon name="newspaper" type="FontAwesome5" />
-							<NBText>Headlines</NBText>
 						</Button>
 						<Button
 							vertical
 							active={props.navigation.state.index === 2}
-							onPress={() => props.navigation.navigate('Twitter')}
-						>
+							onPress={() =>
+								props.navigation.navigate('Twitter')
+							}>
 							<Icon
 								ios="twitter"
 								android="twitter"
 								type="FontAwesome"
 							/>
-							<NBText>Twitter</NBText>
 						</Button>
 					</FooterTab>
 				</Footer>
 			)
-		}
-	}
+		},
+	},
 )
