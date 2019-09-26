cc242377b18e883fe251429420de6b709dd262f1
shrijan-siris <shrijan.siristechnology@gmail.com>
2019-09-26 18:39:58 +0545

    nep-78-refresh-btn: refresh btn in splash screen when internet is off

--- app.js
+++ app.js
@@ -1,37 +1,49 @@
-import React, { Component } from 'react'
-import { StatusBar } from 'react-native'
 import { Provider } from 'react-redux'
+import { StatusBar, Text } from 'react-native'
 import { StyleProvider, Root } from 'native-base'
+import React, { useEffect, useState } from 'react'
+import SplashScreen from 'react-native-splash-screen'
+import { useNetInfo } from '@react-native-community/netinfo'
+
+import { store } from './src/store'
 import AppContainer from './src/frame/app-container'
+import ErrorBoundary from './src/error/error-boundry'
+import { SplashScreenComponent } from './src/components'
 import getTheme from './src/native-base-theme/components'
 import variables from './src/native-base-theme/variables/platform'
-import { store } from './src/store'
-import ErrorBoundary from './src/error/error-boundry'
-import SplashScreen from 'react-native-splash-screen'
 
-export default class App extends Component {
-	constructor() {
-		super()
-		this.state = {
-			isLoading: true,
-		}
-	}
-	componentDidMount = () => {
+function App() {
+	const netInfo = useNetInfo()
+	const [isConnected, setConnected] = useState(true)
+	const [refreshCounter, setRefreshCounter] = useState(0)
+	useEffect(() => {
 		SplashScreen.hide()
+	}, [])
+
+	useEffect(() => {
+		setConnected(netInfo.isConnected)
+	}, [netInfo])
+
+	const refreshApp = () => {
+		setRefreshCounter(refreshCounter + 1)
 	}
 
-	render() {
-		return (
-			<StyleProvider style={getTheme(variables)}>
-				<Provider store={store}>
-					<StatusBar barStyle="light-content" />
-					<Root>
-						<ErrorBoundary>
-							<AppContainer />
-						</ErrorBoundary>
-					</Root>
-				</Provider>
-			</StyleProvider>
+	const renderContent = () =>
+		isConnected ? (
+			<AppContainer />
+		) : (
+			<SplashScreenComponent onRefresh={refreshApp} />
 		)
-	}
+	return (
+		<StyleProvider style={getTheme(variables)}>
+			<Provider store={store}>
+				<StatusBar barStyle="light-content" />
+				<Root>
+					<ErrorBoundary>{renderContent()}</ErrorBoundary>
+				</Root>
+			</Provider>
+		</StyleProvider>
+	)
 }
+
+export default App
