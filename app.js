7ef57df290cf29a4a712fe1ae655da9c16e2325d
shrijan-siris <shrijan.siristechnology@gmail.com>
2019-10-14 13:19:57 +0545

    nep-110-design-home-screen: theme state s initialized

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import { Provider } from 'react-redux'
 import { StatusBar } from 'react-native'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { StyleProvider, Root } from 'native-base'
 import SplashScreen from 'react-native-splash-screen'
 
@@ -8,19 +8,23 @@ import { store } from './src/store'
 import AppContainer from './src/frame/app-container'
 import ErrorBoundary from './src/error/error-boundry'
 import getTheme from './src/native-base-theme/components'
+import { ApplicationProvider } from 'react-native-ui-kitten'
 import variables from './src/native-base-theme/variables/platform'
 
-import { mapping, light as lightTheme } from '@eva-design/eva'
-import { ApplicationProvider } from 'react-native-ui-kitten'
+/* diable-eslint-line */
+import { mapping, light as lightTheme, dark } from '@eva-design/eva'
 
 function App() {
 	useEffect(() => {
 		SplashScreen.hide()
 	}, [])
 
+	/* diable-eslint-line */
+	const [theme, setTheme] = useState(lightTheme)
+
 	return (
 		<StyleProvider style={getTheme(variables)}>
-			<ApplicationProvider mapping={mapping} theme={lightTheme}>
+			<ApplicationProvider mapping={mapping} theme={theme}>
 				<Provider store={store}>
 					<StatusBar barStyle="light-content" />
 					<Root>
