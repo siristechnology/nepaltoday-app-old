d3db6005477ba76890840afbdae3dc619163782e
Suraj Shrestha <syuraj@gmail.com>
2020-02-08 01:49:57 -0500

    switching from ubuntu to macos for building

--- rename_package.sh
+++ rename_package.sh
@@ -4,7 +4,7 @@ PACKAGE_NAME="com.siristechnology.nepaltodayapp.beta"
 PROJECT_NAME=nepaltodayapp
 BUILD_GRADLE=./android/app/build.gradle
 GOOGLE_SERVICES_FILE=./android/app/google-services.json
-INFO_PLIST_FILE=ios/$PROJECT_NAME/Info.plist
+# INFO_PLIST_FILE=ios/$PROJECT_NAME/Info.plist
 
 sed -i '' 's/applicationId "[^"]*"/applicationId "'$PACKAGE_NAME'"/' $BUILD_GRADLE
 
