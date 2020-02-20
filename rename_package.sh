a73b1fb84f222aa89f161394af6559ae4de4d0d1
Suraj Shrestha <syuraj@gmail.com>
2020-02-19 20:53:18 -0500

    use timestamp as versioncode to publish to google play

--- rename_package.sh
+++ rename_package.sh
@@ -3,12 +3,14 @@
 PACKAGE_NAME="com.siristechnology.nepaltodayapp.beta"
 PROJECT_NAME=nepaltodayapp
 BETA_APP_NAME="NepalToday-Beta"
+TIME_STAMP=$(date +%s)
 BUILD_GRADLE=./android/app/build.gradle
 ANDROID_STRINGS_FILE=./android/app/src/main/res/values/strings.xml
 GOOGLE_SERVICES_FILE=./android/app/google-services.json
 # INFO_PLIST_FILE=ios/$PROJECT_NAME/Info.plist
 
 sed -i '' 's/applicationId "[^"]*"/applicationId "'$PACKAGE_NAME'"/' $BUILD_GRADLE
+sed -i '' 's/versionCode .*$/versionCode '$TIME_STAMP'/' $BUILD_GRADLE
 
 sed -i '' 's/"app_name">[^<]*</"app_name">'$BETA_APP_NAME'</' $ANDROID_STRINGS_FILE
 
