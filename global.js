c5555d82fc237ef77f891764b6aecb6fd201af79
Suraj Shrestha <syuraj@gmail.com>
2020-03-08 15:36:18 -0400

    upgrading packages

--- global.js
+++
@@ -1,4 +0,0 @@
-export default {
-	nepalTodayServer: 'https://nepaltoday-api-qa.herokuapp.com/graphql',
-	weatherAPI_APPID: '25e02e338ce3a39c75e5f2595a881e3d',
-}
--- src/frame/app-container.js
+++ src/frame/app-container.js
@@ -1,4 +1,5 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation'
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
 
 import { ArticleWebviewComponent, SplashScreenComponent } from '../components'
 import { BottomTabScreen } from '../screens/navigation/bottom-navigation.screen'
