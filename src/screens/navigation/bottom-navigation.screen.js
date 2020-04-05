07828d0d25ef41d119e12e01d623d3a39f8ce375
Suraj Shrestha <syuraj@gmail.com>
2020-04-05 12:25:16 -0400

    remove unused code

--- src/screens/navigation/bottom-navigation.screen.js
+++ src/screens/navigation/bottom-navigation.screen.js
@@ -1,28 +1,11 @@
 import React from 'react'
 import FontAwesome from 'react-native-vector-icons/FontAwesome5'
 import { createBottomTabNavigator } from 'react-navigation-tabs'
-import { BottomNavigation, BottomNavigationTab } from 'react-native-ui-kitten'
 
 import HomeScreen from '../home-screen/home.screen'
 import TwitterScreen from '../twitter-screen/twitter.screen'
 import HeadlineScreen from '../headline-screen/headline.screen'
 import CoronaStatsScreen from '../corona-stats-screen'
-
-export const ButtonNavigationComponent = (props) => {
-	const onTabSelect = (selectedIndex) => {
-		const { [selectedIndex]: selectedRoute } = props.navigation.state.routes
-		props.navigation.navigate(selectedRoute.routeName)
-	}
-
-	return (
-		<BottomNavigation selectedIndex={props.navigation.state.index} onSelect={onTabSelect}>
-			<BottomNavigationTab title="Home" />
-			<BottomNavigationTab title="Headline" />
-			<BottomNavigationTab title="CoronaStats" />
-			<BottomNavigationTab title="Twitter" />
-		</BottomNavigation>
-	)
-}
 
 export const BottomTabScreen = createBottomTabNavigator(
 	{
