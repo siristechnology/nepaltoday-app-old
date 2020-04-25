4827805a033f255cff418145a519eeff83643323
Suraj Shrestha <syuraj@gmail.com>
2020-04-24 20:16:20 -0400

    add link to share msg

--- src/layout/article/article-detail/article-detail.component.tsx
+++ src/layout/article/article-detail/article-detail.component.tsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import { ImageBackground, View, Share } from 'react-native'
-import {
-	ThemedComponentProps,
-	ThemeType,
-	withStyles,
-} from 'react-native-ui-kitten/theme'
+import { ThemedComponentProps, ThemeType, withStyles } from 'react-native-ui-kitten/theme'
 import { Avatar, Text, Button } from 'react-native-ui-kitten/ui'
 import AntDesign from 'react-native-vector-icons/AntDesign'
 
@@ -21,10 +17,7 @@ interface ComponentProps {
 
 export type ArticleDetailComponentProps = ThemedComponentProps & ComponentProps
 
-class ArticleDetailComponent extends React.Component<
-	ArticleDetailComponentProps
-	> {
-
+class ArticleDetailComponent extends React.Component<ArticleDetailComponentProps> {
 	public render(): React.ReactNode {
 		const { themedStyle, article } = this.props
 		const { READ_MORE } = np.public
@@ -35,7 +28,7 @@ class ArticleDetailComponent extends React.Component<
 				color="grey"
 				onPress={this.navigateBack}
 				style={{
-					padding: 8
+					padding: 8,
 				}}
 			/>
 		)
@@ -46,7 +39,7 @@ class ArticleDetailComponent extends React.Component<
 				color="grey"
 				onPress={this.shareButtonClick}
 				style={{
-					padding: 8
+					padding: 8,
 				}}
 			/>
 		)
@@ -57,14 +50,8 @@ class ArticleDetailComponent extends React.Component<
 					{BackIcon}
 					{shareButton}
 				</View>
-				<ImageBackground
-					style={themedStyle.image}
-					source={{ uri: article.imageLink }}>
-					<Avatar
-						style={themedStyle.authorPhoto}
-						size="large"
-						source={{ uri: article.source.logoLink }}
-					/>
+				<ImageBackground style={themedStyle.image} source={{ uri: article.imageLink }}>
+					<Avatar style={themedStyle.authorPhoto} size="large" source={{ uri: article.source.logoLink }} />
 				</ImageBackground>
 
 				<View style={themedStyle.detailsContainer}>
@@ -74,10 +61,7 @@ class ArticleDetailComponent extends React.Component<
 					<ArticleActivityBar>
 						<View style={themedStyle.dateContainer}>
 							{ClockIconOutline(themedStyle.dateIcon)}
-							<Text
-								style={themedStyle.dateLabel}
-								appearance="hint"
-								category="p2">
+							<Text style={themedStyle.dateLabel} appearance="hint" category="p2">
 								{getRelativeTime(article.publishedDate)}
 							</Text>
 						</View>
@@ -86,9 +70,7 @@ class ArticleDetailComponent extends React.Component<
 						{article.content}
 					</Text>
 					<View style={themedStyle.readMoreBtnWrapper}>
-						<Button
-							onPress={this.handleLinkClick}
-							style={themedStyle.readMoreBtn}>
+						<Button onPress={this.handleLinkClick} style={themedStyle.readMoreBtn}>
 							{READ_MORE}
 						</Button>
 					</View>
@@ -104,9 +86,9 @@ class ArticleDetailComponent extends React.Component<
 	private shareButtonClick = () => {
 		const { title, link } = this.props.article
 		Share.share({
-			message: title,
+			message: title + '  ' + link,
 			url: link,
-			title: title
+			title: title,
 		})
 	}
 
@@ -116,68 +98,65 @@ class ArticleDetailComponent extends React.Component<
 	}
 }
 
-export const ArticleDetail = withStyles(
-	ArticleDetailComponent,
-	(theme: ThemeType) => ({
-		container: {
-			flex: 1,
-			backgroundColor: theme['background-basic-color-1'],
-		},
-		detailsContainer: {
-			paddingHorizontal: 24,
-			paddingVertical: 4,
-		},
-		dateContainer: {
-			flexDirection: 'row',
-			alignItems: 'center',
-		},
-		image: {
-			minHeight: 175,
-			borderWidth: 1,
-			borderColor: '#f5f7fa',
-		},
-		authorPhoto: {
-			position: 'absolute',
-			left: 24,
-			bottom: -32,
-			margin: 0,
-			borderWidth: 2,
-			borderColor: theme['border-basic-color-2'],
-		},
-		titleLabel: {
-			marginTop: 30,
-			...textStyle.headline,
-			fontSize: 22,
-		},
-		contentLabel: {
-			flex: 1,
-			marginVertical: 12,
-			...textStyle.paragraph,
-			fontSize: 18,
-		},
-		dateIcon: {
-			width: 16,
-			height: 16,
-			tintColor: theme['text-hint-color'],
-		},
-		dateLabel: {
-			marginLeft: 4,
-			...textStyle.paragraph,
-		},
-		headerStyle: {
-			justifyContent: 'space-between',
-			flexDirection: 'row',
-			paddingLeft: 10,
-			paddingRight: 10
-		},
-		readMoreBtnWrapper: {
-			display: 'flex',
-			alignItems: 'center',
-			justifyContent: 'center',
-			margin: 8,
-		},
-		readMoreBtn: {
-			width: 200,
-		},
-	}),
-)
+export const ArticleDetail = withStyles(ArticleDetailComponent, (theme: ThemeType) => ({
+	container: {
+		flex: 1,
+		backgroundColor: theme['background-basic-color-1'],
+	},
+	detailsContainer: {
+		paddingHorizontal: 24,
+		paddingVertical: 4,
+	},
+	dateContainer: {
+		flexDirection: 'row',
+		alignItems: 'center',
+	},
+	image: {
+		minHeight: 175,
+		borderWidth: 1,
+		borderColor: '#f5f7fa',
+	},
+	authorPhoto: {
+		position: 'absolute',
+		left: 24,
+		bottom: -32,
+		margin: 0,
+		borderWidth: 2,
+		borderColor: theme['border-basic-color-2'],
+	},
+	titleLabel: {
+		marginTop: 30,
+		...textStyle.headline,
+		fontSize: 22,
+	},
+	contentLabel: {
+		flex: 1,
+		marginVertical: 12,
+		...textStyle.paragraph,
+		fontSize: 18,
+	},
+	dateIcon: {
+		width: 16,
+		height: 16,
+		tintColor: theme['text-hint-color'],
+	},
+	dateLabel: {
+		marginLeft: 4,
+		...textStyle.paragraph,
+	},
+	headerStyle: {
+		justifyContent: 'space-between',
+		flexDirection: 'row',
+		paddingLeft: 10,
+		paddingRight: 10,
+	},
+	readMoreBtnWrapper: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+		margin: 8,
+	},
+	readMoreBtn: {
+		width: 200,
+	},
+}))
