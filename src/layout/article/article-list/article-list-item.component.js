329dcfaaedacb368fa594f69222db3718152cb47
Suraj Shrestha <syuraj@gmail.com>
2020-05-16 19:35:03 -0400

    using react.memo in headline page

--- src/layout/article/article-list/article-list-item.component.js
+++ src/layout/article/article-list/article-list-item.component.js
@@ -7,23 +7,15 @@ import { getRelativeTime } from '../../../helper/time'
 import { ArticleActivityBar } from '../../../components/articles'
 import { ActivityAuthoring, textStyle } from '../../../components/common'
 
-const ArticleListItemComponent = props => {
+const ArticleListItemComponent = React.memo((props) => {
 	const { style, themedStyle, article, ...restProps } = props
 	const onPress = () => {
 		props.onPress(article)
 	}
 
 	return (
-		<TouchableOpacity
-			activeOpacity={0.95}
-			{...restProps}
-			style={[themedStyle.container, style]}
-			onPress={onPress}>
-			<ImageBackground
-				style={themedStyle.imageContainer}
-				imageStyle={themedStyle.image}
-				source={{ uri: article.imageLink }}
-			/>
+		<TouchableOpacity activeOpacity={0.95} {...restProps} style={[themedStyle.container, style]} onPress={onPress}>
+			<ImageBackground style={themedStyle.imageContainer} imageStyle={themedStyle.image} source={{ uri: article.imageLink }} />
 			<ArticleActivityBar style={themedStyle.activityContainer}>
 				<ActivityAuthoring
 					photo={{ uri: article.source.logoLink }}
@@ -35,19 +27,15 @@ const ArticleListItemComponent = props => {
 				<Text style={themedStyle.titleLabel} category="h5">
 					{article.title}
 				</Text>
-				<Text
-					style={themedStyle.descriptionLabel}
-					appearance="hint"
-					category="s1">
-					{article.shortDescription
-						? article.shortDescription.substring(0, 100) + '...'
-						: ''}
+				<Text style={themedStyle.descriptionLabel} appearance="hint" category="s1">
+					{article.shortDescription ? article.shortDescription.substring(0, 100) + '...' : ''}
 				</Text>
 			</View>
 		</TouchableOpacity>
 	)
-}
-export const ArticleListItem = withStyles(ArticleListItemComponent, theme => ({
+})
+
+export const ArticleListItem = withStyles(ArticleListItemComponent, (theme) => ({
 	container: {
 		borderRadius: 12,
 	},
--- src/layout/headline/tab-view/headline-list.component.js
+++ src/layout/headline/tab-view/headline-list.component.js
@@ -4,24 +4,12 @@ import { List } from 'react-native-ui-kitten/ui'
 import { withStyles } from 'react-native-ui-kitten/theme'
 import { ArticleListItem } from '../../article/article-list/article-list-item.component'
 
-const HeadlineListComponent = ({
-	articles,
-	onItemPress,
-	themedStyle,
-	refreshing,
-	handleRefresh,
-}) => {
-	const _onItemPress = article => {
+const HeadlineListComponent = React.memo(({ articles, onItemPress, themedStyle, refreshing, handleRefresh }) => {
+	const _onItemPress = (article) => {
 		onItemPress(article)
 	}
-	const renderItem = info => {
-		return (
-			<ArticleListItem
-				style={themedStyle.item}
-				article={info.item}
-				onPress={() => _onItemPress(info.item)}
-			/>
-		)
+	const renderItem = (info) => {
+		return <ArticleListItem style={themedStyle.item} article={info.item} onPress={() => _onItemPress(info.item)} />
 	}
 
 	return (
@@ -29,19 +17,13 @@ const HeadlineListComponent = ({
 			contentContainerStyle={themedStyle.container}
 			data={articles}
 			renderItem={renderItem}
-			keyExtractor={item => item._id}
-			refreshControl={
-				<RefreshControl
-					refreshing={refreshing}
-					onRefresh={handleRefresh}
-					colors={['#0000ff', '#689F38']}
-				/>
-			}
+			keyExtractor={(item) => item._id}
+			refreshControl={<RefreshControl refreshing={refreshing} onRefresh={handleRefresh} colors={['#0000ff', '#689F38']} />}
 		/>
 	)
-}
+})
 
-export const HeadlineList = withStyles(HeadlineListComponent, theme => ({
+export const HeadlineList = withStyles(HeadlineListComponent, (theme) => ({
 	container: {
 		paddingHorizontal: 16,
 		paddingVertical: 8,
