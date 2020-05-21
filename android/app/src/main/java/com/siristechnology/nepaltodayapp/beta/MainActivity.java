7e1e95db5fb60415a16aa0ccb09de68eb130f33a
Suraj Shrestha <syuraj@gmail.com>
2020-05-20 20:40:47 -0400

    hot reloading fix (#112)
    * copying new android folder from new react-native project
    
    * reenabling crashlytics
    
    * minor refactor

--- android/app/src/main/java/com/siristechnology/nepaltodayapp/beta/MainActivity.java
+++ android/app/src/main/java/com/siristechnology/nepaltodayapp/beta/MainActivity.java
@@ -1,46 +1,15 @@
 package com.siristechnology.nepaltodayapp.beta;
 
-import android.content.res.Configuration;
-import android.os.Build;
-import android.os.Bundle;
 import com.facebook.react.ReactActivity;
-import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.ReactRootView;
-import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
-import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
-	/**
-	 * Returns the name of the main component registered from JavaScript. This is
-	 * used to schedule rendering of the component.
-	 */
-	@Override
-	protected String getMainComponentName() {
-		return "nepaltodayapp";
-	}
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		SplashScreen.show(this, true);
-		super.onCreate(savedInstanceState);
-	}
-
-	@Override
-	protected ReactActivityDelegate createReactActivityDelegate() {
-		return new ReactActivityDelegate(this, getMainComponentName()) {
-			@Override
-			protected ReactRootView createRootView() {
-				return new RNGestureHandlerEnabledRootView(MainActivity.this);
-			}
-		};
-	}
-
-	@Override
-    public void applyOverrideConfiguration(Configuration overrideConfiguration) {
-        if (Build.VERSION.SDK_INT >= 21 && Build.VERSION.SDK_INT <= 25) {
-            return;
-        }
-        super.applyOverrideConfiguration(overrideConfiguration);
-    }
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "nepaltodayapp";
+  }
 }
