f5367de74d7bac0995155d458c80f2d47eed0e61
Suraj Shrestha <syuraj@gmail.com>
2020-05-22 01:46:13 -0400

    minor refactor

--- prepare_release.sh
+++ prepare_release.sh
@@ -8,13 +8,12 @@ ANDROID_STRINGS_FILE=./android/app/src/main/res/values/strings.xml
 ANDROID_MANIFEST_FILE=./android/app/src/main/AndroidManifest.xml
 GOOGLE_SERVICES_FILE=./android/app/google-services.json
 
-sed -i '' 's/applicationId "[^"]*"/applicationId "'$PACKAGE_NAME'"/' $BUILD_GRADLE
-sed -i '' 's/versionCode .*$/versionCode '$TIME_STAMP'/' $BUILD_GRADLE
-sed -i '' 's/"app_name">[^<]*</"app_name">'$APP_NAME'</' $ANDROID_STRINGS_FILE
+sed -i 's/applicationId "[^"]*"/applicationId "'$PACKAGE_NAME'"/' $BUILD_GRADLE
+sed -i 's/versionCode .*$/versionCode '$TIME_STAMP'/' $BUILD_GRADLE
+sed -i 's/"app_name">[^<]*</"app_name">'$APP_NAME'</' $ANDROID_STRINGS_FILE
 
-sed -i '' 's/"@mipmap\/ic_launcher_prod"/"@mipmap\/ic_launcher"/' $ANDROID_MANIFEST_FILE
+sed -i 's/"@mipmap\/ic_launcher"/"@mipmap\/ic_launcher_prod"/' $ANDROID_MANIFEST_FILE
 
-sed -i '' 's/"package_name": "[^"]*"/"package_name": "'$PACKAGE_NAME'"/' $GOOGLE_SERVICES_FILE
 
 mv .env.release .env
 mv ./android/app/google-services.json.prod ./android/app/google-services.json
