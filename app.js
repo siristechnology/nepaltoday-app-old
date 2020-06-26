e8ef55b9ba9a95b759dbcfdb6afb0628ae389d66
Shiva Aryal <lostinhell99@gmail.com>
2020-06-26 09:25:07 +0545

    NEP-184 : Notification click done and comments removed (#116)
    * fixed on click the bottom tab scroll to top
    
    * Article detail title position fixed, on notification click go to article detail directly without going to home screen
    
    * NotificationClickBranch
    
    Co-authored-by: Shiva Aryal <shivaaryalj7@gmail.com>

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Provider } from 'react-redux'
 import { StatusBar } from 'react-native'
 import SplashScreen from 'react-native-splash-screen'
@@ -12,24 +12,46 @@ import notificationHandler from './src/services/notification-handler'
 import NavigationService from './src/services/navigationService'
 import crashlytics from '@react-native-firebase/crashlytics'
 import auth from '@react-native-firebase/auth'
+import AppLayout from './src/frame/app-layout'
+import { CircularSpinner } from './src/components/common'
 
 const App = () => {
+
+	const [clicked, setClicked] = useState(false)
+	const [loading, setLoading] = useState(false)
+	const [article, setArticle] = useState({})
 	useEffect(() => {
 		SplashScreen.hide()
-
+		setLoading(true)
 		signInAnonymously().then(() => notificationHandler.register(auth().currentUser))
-	}, [])
+		notificationHandler.checkForNotification().then(res=>{
+			setClicked(true)
+			setLoading(false)
+			setArticle(res.data.getArticle)
+
+		}).catch(err=>{
+			setLoading(false)
+		})
+
+	}, [clicked])
 
 	return (
 		<ApplicationProvider mapping={mapping} theme={lightTheme}>
 			<Provider store={store}>
 				<StatusBar barStyle="light-content" />
 				<ErrorBoundary>
+					{loading && <AppLayout>
+						<CircularSpinner/>	
+					</AppLayout> ||
 					<AppContainer
 						ref={(navigatorRef) => {
-							NavigationService.setTopLevelNavigator(navigatorRef)
+							if(!clicked){
+								NavigationService.setTopLevelNavigator(navigatorRef)
+							}else{
+								NavigationService.setTopLevelNavigator(navigatorRef,'ArticleDetail', { article: article })
+							}
 						}}
-					/>
+					/>}
 				</ErrorBoundary>
 			</Provider>
 		</ApplicationProvider>
