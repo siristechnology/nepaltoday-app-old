b663e5e5dbe1de02892513e3a49cdbe8828d1518
Shiva Aryal <lostinhell99@gmail.com>
2020-09-24 14:37:38 +0545

    NEP-237 : Corona source (#162)
    * corona source added
    
    * international corona source added and back button in webview
    
    Co-authored-by: Shiva Aryal <shivaaryalj7@gmail.com>

--- src/components/article-webview.component.js
+++ src/components/article-webview.component.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import { AppState, StyleSheet } from 'react-native'
+import { AppState, StyleSheet, Text, View } from 'react-native'
 import { Layout, Spinner } from 'react-native-ui-kitten'
 import { WebView } from 'react-native-webview'
+import AntDesign from 'react-native-vector-icons/AntDesign'
 
-export const ArticleWebviewComponent = ({ navigation }) => {
+export const ArticleWebviewComponent = ({navigation}) => {
 	const link = navigation.getParam('link')
 	const [appState, setAppState] = useState(AppState.currentState)
 
@@ -18,25 +19,42 @@ export const ArticleWebviewComponent = ({ navigation }) => {
 		setAppState(nextAppState)
 	}
 
+	const BackIcon = (
+		<AntDesign
+			name="back"
+			size={24}
+			color="grey"
+			onPress={()=>navigation.goBack()}
+			style={{
+				padding: 8,
+			}}
+		/>
+	)
+
 	if (appState == 'active') {
 		return (
-			<WebView
-				renderLoading={() => (
-					<Layout style={styles.container}>
-						<Spinner />
-					</Layout>
-				)}
-				mediaPlaybackRequiresUserAction={true}
-				mixedContentMode="always"
-				allowsInlineMediaPlayback="false"
-				startInLoadingState
-				source={{ uri: link }}
-				style={{
-					flex: 1,
-					alignItems: 'center',
-					justifyContent: 'center',
-				}}
-			/>
+			<View style={{flex:1}}>
+				<View style={{paddingLeft:10}}>
+					{BackIcon}
+				</View>
+				<WebView
+					renderLoading={() => (
+						<Layout style={styles.container}>
+							<Spinner />
+						</Layout>
+					)}
+					mediaPlaybackRequiresUserAction={true}
+					mixedContentMode="always"
+					allowsInlineMediaPlayback="false"
+					startInLoadingState
+					source={{ uri: link }}
+					style={{
+						flex: 1,
+						alignItems: 'center',
+						justifyContent: 'center',
+					}}
+				/>
+			</View>
 		)
 	} else {
 		return null
--- src/screens/corona-screen/coronaDistrictStats/districtList.js
+++ src/screens/corona-screen/coronaDistrictStats/districtList.js
@@ -79,6 +79,11 @@ const DistrictList = () => {
 				keyExtractor={(item) => item.name}
 				contentContainerStyle={styles.container}
 				refreshControl={<RefreshControl refreshing={refreshing} onRefresh={handleRefresh} colors={['#0000ff', '#689F38']} />}
+				ListFooterComponent={
+					<Text style={styles.sourceText}>
+						{data && data.getDistrictCoronaStats && data.getDistrictCoronaStats.source}
+					</Text>
+				}
 			/>
 		</AppLayout>
 	)
@@ -120,9 +125,9 @@ const styles = StyleSheet.create({
 		paddingHorizontal: 15,
 	},
 	sourceText: {
-		alignSelf: 'flex-end',
+		alignSelf: 'center',
 		marginRight: 10,
-		marginTop: 3,
+		marginBottom: 5,
 		fontSize: 13,
 	},
 	listContainer: {
