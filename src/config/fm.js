2fb06fb2f8176bf43c5488a8d8726038d125f92d
Shiva Aryal <lostinhell99@gmail.com>
2020-09-30 20:09:54 +0545

    NEP-269 : fm radio integrated in our app (#166)
    * NEP-269 : fm radio integrated in our app
    
    * NEP-269 : deep scan error corrected
    
    * NEP-269 : lgtm issue fixed
    
    Co-authored-by: Shiva Aryal <shivaaryalj7@gmail.com>
    Co-authored-by: Suraj Shrestha <syuraj@gmail.com>

---
+++ src/config/fm.js
@@ -0,0 +1,157 @@
+export const fmDetails = [{
+    id: '1',
+    url: 'http://broadcast.radiokantipur.com:7248/stream/;',
+    title: 'Radio Kantipur',
+    artist: '96.1',
+    artwork: 'https://static-media.streema.com/media/cache/5b/fd/5bfdbd66a39028a95e5f819a897d572a.jpg'
+  },{
+    id: '2',
+    url: 'http://kalika-stream.softnep.com:7740/;',
+    title: 'Kakila FM',
+    artist: '95.2',
+    artwork: 'https://static-media.streema.com/media/cache/da/3b/da3ba4dd0ed160d8430d2e09c3c40eee.jpg'
+  },{
+    id: '3',
+    url: 'http://198.89.123.112:9322/stream/;',
+    title: 'Radio Sagarmatha',
+    artist: '102.4',
+    artwork: 'https://static-media.streema.com/media/cache/c9/6b/c96be3f5548ff7ef4b025ade0ccf70d2.jpg'
+  },{
+    id: '4',
+    url: 'http://stream.zenolive.com/wtuvp08xq1duv',
+    title: 'Ujyalo 90 Network',
+    artist: '90',
+    artwork: 'https://static-media.streema.com/media/cache/dc/23/dc23de634f406ce437d531945797af7b.jpg'
+  },{
+    id: '5',
+    url: 'http://live.itechnepal.com:8964/stream',
+    title: 'Radio Annapurna',
+    artist: '93.4',
+    artwork: 'https://static-media.streema.com/media/cache/27/b4/27b4390714bf2fa89d83fe5764c1985c.jpg'
+  },{
+    id: '6',
+    url: 'https://tv-ingest.hamropatro.com/radio-live/classic/icecast.audio',
+    title: 'Classic FM',
+    artist: '101.2',
+    artwork: 'https://static-media.streema.com/media/cache/f8/51/f851869aa5fe256697b1a5f010562826.jpg'
+  },{
+    id: '7',
+    url: 'http://usa15.fastcast4u.com:5042/stream/;',
+    title: 'HITS FM',
+    artist: '91.2',
+    artwork: 'https://static-media.streema.com/media/cache/e3/56/e3565b743b2914f34e36ea34e558f780.jpg'
+  },{
+    id: '8',
+    url: 'http://radionepal.gov.np:40100/stream',
+    title: 'Radio Nepal',
+    artist: 'AM 792',
+    artwork: 'https://static-media.streema.com/media/cache/01/74/0174acc4f5acb6bc6dc0ad986272560d.jpg'
+  },{
+    id: '9',
+    url: 'http://stream.zenolive.com/fvrx47wpg0quv',
+    title: 'Radio Audio',
+    artist: '106.3',
+    artwork: 'https://static-media.streema.com/media/cache/47/b9/47b9af0cacb06c3d04aeb1aab8ca1dcf.jpg'
+  },{
+    id: '10',
+    url: 'http://live.itechnepal.com:8541/stream',
+    title: 'HBC Shakti Radio',
+    artist: '94.1',
+    artwork: 'https://static-media.streema.com/media/cache/e8/c9/e8c936f9ffa59babf67fbe2080bf3780.jpg'
+  },{
+    id: '11',
+    url: 'http://streaming.hamropatro.com:8631/;',
+    title: 'Image FM',
+    artist: '97.9',
+    artwork: 'https://static-media.streema.com/media/cache/82/17/82175cc930ea9dc94e1ab23fab80b3ae.jpg'
+  },{
+    id: '12',
+    url: 'http://streaming.softnep.net:8037/stream/;',
+    title: 'Capital FM',
+    artist: '92.4',
+    artwork: 'https://static-media.streema.com/media/cache/35/86/358631cb25a78959e83a50f8b3c6f2d2.jpg'
+  },{
+    id: '13',
+    url: 'http://streaming.softnep.net:8025/stream/;',
+    title: 'Radio Thaha Sachar',
+    artist: '99.4',
+    artwork: 'https://static-media.streema.com/media/cache/47/45/47450516bfd63304e15acfc09bfae622.jpg'
+  },{
+    id: '14',
+    url: 'http://144.76.110.138:9950/stream/;',
+    title: 'Himal Radio',
+    artist: 'Himal Radio',
+    artwork: 'https://static-media.streema.com/media/cache/83/9f/839f97ebc4cfcbe5515f5be70597af16.jpg'
+  },{
+    id: '15',
+    url: 'http://streaming.hamropatro.com:8230/;',
+    title: 'Nepal FM',
+    artist: '91.8',
+    artwork: 'https://static-media.streema.com/media/cache/67/9c/679c1b7826f260c40e9ded9d8bb0c666.jpg'
+  },{
+    id: '16',
+    url: 'http://stream80.tligradio.org/proxy/tligradio_ne?mp=/',
+    title: 'True Life in God Radio Nepali',
+    artist: 'True Life in God Radio Nepali',
+    artwork: 'https://static-media.streema.com/media/cache/70/1b/701b64ba36362f97cdfd8de44b7f288c.jpg'
+  },{
+    id: '17',
+    url: 'http://live.itech.host:8162/stream',
+    title: 'Good News FM',
+    artist: '105.1',
+    artwork: 'https://static-media.streema.com/media/cache/cb/99/cb99d73c53c39e633f86c2169e4c9def.jpg'
+  },{
+    id: '18',
+    url: 'http://s2.voscast.com:10720/;',
+    title: 'Radio Nagarik',
+    artist: '96.5',
+    artwork: 'https://static-media.streema.com/media/cache/85/c3/85c3482617477c6f2ee1352401ada536.jpg'
+  },{
+    id: '19',
+    url: 'http://streaming.softnep.net:8085/stream/;',
+    title: 'Radio Lumbini',
+    artist: '96.8',
+    artwork: 'https://static-media.streema.com/media/cache/d5/e8/d5e83074b9d01e0439ede3823751a50e.jpg'
+  },{
+    id: '20',
+    url: 'http://209.133.194.178:8293/stream/;',
+    title: 'Tinau FM',
+    artist: '98.2',
+    artwork: 'https://static-media.streema.com/media/cache/12/10/1210ca8b842755eace8b11bb4769ae4c.jpg'
+  },{
+    id: '21',
+    url: 'http://streaming.softnep.net:8031/stream/;',
+    title: 'Radio Triveni',
+    artist: '100.6',
+    artwork: 'https://static-media.streema.com/media/cache/7c/77/7c77f8be4f6a7980bff5a7a597d3438d.jpg'
+  },{
+    id: '22',
+    url: 'http://stream.zenolive.com/v3e78tqm5gruv',
+    title: '24 Nepali Online Radio',
+    artist: '24 Nepali Online Radio',
+    artwork: 'https://static-media.streema.com/media/cache/97/6e/976e2561330de8db240139730e9fcc47.jpg'
+  },{
+    id: '23',
+    url: 'http://stream.zenolive.com/2w81t82wx3duv',
+    title: 'Radio Aadhikhola',
+    artist: '105.4',
+    artwork: 'https://static-media.streema.com/media/cache/4b/ee/4bee9e347cffefc899603a5ad46d85a6.jpg'
+  },{
+    id: '24',
+    url: 'http://live.itechnepal.com:9108/stream?type=.mp3',
+    title: 'Radio Bihani',
+    artist: '97.6',
+    artwork: 'https://static-media.streema.com/media/cache/d3/35/d335935b9aba43f7c1a4279d83adc116.jpg'
+  },{
+    id: '25',
+    url: 'http://betelgeuse.dribbcast.com/proxy/radiopariwartan?mp=/stream',
+    title: 'Radio Pariwartan',
+    artist: '89.4',
+    artwork: 'https://static-media.streema.com/media/cache/9a/e5/9ae5a2adcaf2fe7b2e67be50538d267b.jpg'
+  },{
+    id: '26',
+    url: 'http://streaming.hamropatro.com:8603/;',
+    title: 'Chitawan Online FM',
+    artist: 'Chitawan Online FM',
+    artwork: 'https://static-media.streema.com/media/cache/9a/e5/9ae5a2adcaf2fe7b2e67be50538d267b.jpg'
+}]
---
+++ src/screens/radio-screen/radio.screen.js
@@ -0,0 +1,108 @@
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, View, Text } from 'react-native'
+import AppLayout from '../../frame/app-layout'
+import TrackPlayer, {
+    TrackPlayerEvents,
+    STATE_PLAYING,
+} from 'react-native-track-player';
+import {useTrackPlayerEvents} from 'react-native-track-player/lib/hooks';
+
+import RadioListContainer from './components/radioListContainer'
+
+import { fmDetails } from './../../config/fm'
+import BottomPlayer from './components/bottomPlayer';
+
+TrackPlayer.registerPlaybackService(() => require('./service.js'));
+
+const trackPlayerInit = async () => {
+    await TrackPlayer.setupPlayer();
+    TrackPlayer.updateOptions({
+      stopWithApp: true,
+      capabilities: [
+        TrackPlayer.CAPABILITY_PLAY,
+        TrackPlayer.CAPABILITY_PAUSE,
+        TrackPlayer.CAPABILITY_STOP,
+        TrackPlayer.CAPABILITY_SKIP_TO_NEXT,
+        TrackPlayer.CAPABILITY_SKIP_TO_PREVIOUS
+      ],
+    });
+    for(let fmDetail of fmDetails){
+      await TrackPlayer.add({
+        id: fmDetail.id,
+        url: fmDetail.url,
+        type: 'default',
+        title: fmDetail.title,
+        album: fmDetail.album,
+        artist: fmDetail.artist,
+        artwork: fmDetail.artwork,
+      });
+    }
+    return true;
+};
+
+const RadioScreen = () => {
+
+    // const [isTrackPlayerInit, setIsTrackPlayerInit] = useState(false);
+
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    // const [currentChannel, setCurrentChannel] = useState({})
+
+    const onFMSelect = (channel) => {
+        setCurrentChannel(channel)
+        TrackPlayer.play();
+        TrackPlayer.skip(channel.id)
+        setIsPlaying(true);
+        // TrackPlayer.pause();
+        // setIsPlaying(false);
+    }
+
+    useEffect(() => {
+        const startPlayer = async () => {
+          let isInit =  await trackPlayerInit();
+        //   setIsTrackPlayerInit(isInit);
+            console.log(isInit)
+        }
+        startPlayer();
+    }, []);
+
+    useTrackPlayerEvents([TrackPlayerEvents.PLAYBACK_STATE], event => {
+        if (event.state === STATE_PLAYING) {
+          setIsPlaying(true);
+        } else {
+          setIsPlaying(false);
+        }
+    });
+
+    return(
+        <AppLayout>
+            <View style={style.headerStyle}>
+                <Text style={style.headerText}>
+                    NepalToday FM
+                </Text>
+            </View>
+            <RadioListContainer
+                fmList={fmDetails}
+                onFMSelect={onFMSelect}
+            />
+            <BottomPlayer
+                isPlaying={isPlaying}
+            />
+        </AppLayout>
+    )
+}
+
+const style = StyleSheet.create({
+    headerStyle: {
+        paddingHorizontal: 20,
+        paddingBottom: 10,
+        backgroundColor: '#fff',
+    },
+    headerText: {
+        fontWeight: 'bold',
+		fontSize: 26,
+		paddingTop: 5
+    }
+})
+
+export default RadioScreen
