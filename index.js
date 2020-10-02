5c1345ba1731473ddd034db8029a6f22ab97f494
Suraj Shrestha <syuraj@gmail.com>
2020-10-02 16:26:42 -0400

    upgrade java

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 import React from 'react'
-import { AppRegistry, YellowBox } from 'react-native'
+import { AppRegistry, LogBox } from 'react-native'
 import App from './app.js'
 import { name as appName } from './app.json'
 import { ApolloProvider } from '@apollo/react-hooks'
 import GraphqlClient from './src/graphql/graphql-client'
 
-YellowBox.ignoreWarnings([
-	'Warning: componentWillMount is deprecated',
-	'Warning: componentWillReceiveProps is deprecated',
-	'Module RCTImageLoader requires',
-])
+LogBox.ignoreLogs(['Warning: componentWillMount is deprecated', 'Warning: componentWillReceiveProps is deprecated', 'Module RCTImageLoader requires'])
 
 const ApolloApp = () => (
 	<ApolloProvider client={GraphqlClient}>
