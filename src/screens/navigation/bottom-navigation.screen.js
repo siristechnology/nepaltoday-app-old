dd2959de94c80eea1ab3950d646c1dc487bbe999
Suraj Shrestha <syuraj@gmail.com>
2020-10-20 00:35:53 -0400

    using material-bottom-tabs

--- src/screens/navigation/bottom-navigation.screen.js
+++ src/screens/navigation/bottom-navigation.screen.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import FontAwesome from 'react-native-vector-icons/FontAwesome5'
-import SimpleLine from 'react-native-vector-icons/SimpleLineIcons'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 
 import HomeScreen from '../home-screen/home.screen'
 import TwitterScreen from '../twitter-screen/twitter.screen'
@@ -10,47 +9,43 @@ import CoronaScreen from '../corona-screen/corona.screen'
 import TrendingScreen from './../trending-screen/trending.screen'
 import RadioScreen from './../radio-screen/radio.screen'
 
-const Tab = createBottomTabNavigator()
+const Tab = createMaterialBottomTabNavigator()
 
 export const BottomTabScreen = ({ route }) => {
 	return (
-		<Tab.Navigator
-			initialRouteName={route.params.initialScreenName}
-			tabBarOptions={{
-				activeTintColor: '#1da1f2',
-				inactiveTintColor: 'gray',
-				showLabel: false,
-			}}
-		>
+		<Tab.Navigator initialRouteName={route.params.initialScreenName} shifting={false} labeled={false}>
 			<Tab.Screen
 				name="Home"
 				component={HomeScreen}
-				options={{ tabBarIcon: ({ color }) => <FontAwesome name={'home'} size={25} color={color} /> }}
+				options={{ tabBarIcon: ({ color }) => <MaterialCommunityIcons name="home-outline" size={25} color={color} /> }}
 			/>
 			<Tab.Screen
 				name="Headline"
 				component={HeadlineScreen}
-				options={{ tabBarIcon: ({ color }) => <FontAwesome name={'newspaper'} size={25} color={color} /> }}
+				options={{ tabBarIcon: ({ color }) => <MaterialCommunityIcons name="newspaper-variant-multiple-outline" size={25} color={color} /> }}
 			/>
 			<Tab.Screen
 				name="Corona"
 				component={CoronaScreen}
-				options={{ tabBarIcon: ({ color }) => <FontAwesome name={'skull'} size={25} color={color} />, tabBarTestID: 'coronaScreen' }}
+				options={{
+					tabBarIcon: ({ color }) => <MaterialCommunityIcons name="skull" size={25} color={color} />,
+					tabBarTestID: 'coronaScreen',
+				}}
 			/>
 			<Tab.Screen
 				name="Trending"
 				component={TrendingScreen}
-				options={{ tabBarIcon: ({ color }) => <SimpleLine name={'fire'} size={25} color={color} /> }}
+				options={{ tabBarIcon: ({ color }) => <MaterialCommunityIcons name="trending-up" size={25} color={color} /> }}
 			/>
 			<Tab.Screen
 				name="Twitter"
 				component={TwitterScreen}
-				options={{ tabBarIcon: ({ color }) => <FontAwesome name={'twitter'} size={25} color={color} /> }}
+				options={{ tabBarIcon: ({ color }) => <MaterialCommunityIcons name="twitter" size={25} color={color} /> }}
 			/>
 			<Tab.Screen
 				name="Radio"
 				component={RadioScreen}
-				options={{ tabBarIcon: ({ color }) => <FontAwesome name={'headphones'} size={25} color={color} /> }}
+				options={{ tabBarIcon: ({ color }) => <MaterialCommunityIcons name="radio" size={25} color={color} /> }}
 			/>
 		</Tab.Navigator>
 	)
