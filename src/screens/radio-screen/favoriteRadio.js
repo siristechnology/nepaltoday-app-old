1246218dea524050147ba55c80ae21786c902f86
Shiva Aryal <shivaaryalj7@gmail.com>
2020-10-20 12:43:08 +0545

    NEP-273-Radio UI changed slightly

--- src/screens/radio-screen/favoriteRadio.js
+++ src/screens/radio-screen/favoriteRadio.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React from 'react'
 import AppLayout from '../../frame/app-layout'
 import FavoriteRadioListContainer from './components/favoriteRadioListContainer'
 
