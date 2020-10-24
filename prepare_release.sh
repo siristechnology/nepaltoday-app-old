c47482106051462fedb96320d022c19510e54132
Suraj Shrestha <syuraj@gmail.com>
2020-10-24 14:27:57 -0400

    Remove ic_launcher_prod - 1

--- prepare_release.sh
+++ prepare_release.sh
@@ -3,17 +3,19 @@
 PACKAGE_NAME="com.siristechnology.nepaltodayapp"
 APP_NAME="NepalToday"
 TIME_STAMP=$(date +%s)
+IC_LAUNCHER_BACKGROUND="#e7f3ff"
 BUILD_GRADLE=./android/app/build.gradle
 ANDROID_STRINGS_FILE=./android/app/src/main/res/values/strings.xml
 ANDROID_MANIFEST_FILE=./android/app/src/main/AndroidManifest.xml
+IC_LAUNCHER_BACKGROUND_FILE=./android/app/src/main/res/values/ic_launcher_background.xml
+
 GOOGLE_SERVICES_FILE=./android/app/google-services.json
 
 sed -i '' -e 's/applicationId "[^"]*"/applicationId "'$PACKAGE_NAME'"/' $BUILD_GRADLE
 sed -i '' -e 's/versionCode .*$/versionCode '$TIME_STAMP'/' $BUILD_GRADLE
 sed -i '' -e 's/"app_name">[^<]*</"app_name">'$APP_NAME'</' $ANDROID_STRINGS_FILE
 
-sed -i '' -e 's/"@mipmap\/ic_launcher"/"@mipmap\/ic_launcher_prod"/' $ANDROID_MANIFEST_FILE
-
+sed -i '' -e 's/"ic_launcher_background">[^<]*</"ic_launcher_background">'$IC_LAUNCHER_BACKGROUND'</' $IC_LAUNCHER_BACKGROUND_FILE
 
 mv .env.release .env
 mv ./android/app/google-services.json.prod ./android/app/google-services.json
