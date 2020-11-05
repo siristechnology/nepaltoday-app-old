a5af71f879a1f2546809f34fe5502dba1a5149e5
Shiva Aryal <lostinhell99@gmail.com>
2020-11-05 17:22:29 +0545

    NEP-293 : Caching articles in category page (#190)
    Co-authored-by: Shiva Aryal <shivaaryalj7@gmail.com>

--- src/helper/cacheStorage.js
+++ src/helper/cacheStorage.js
@@ -1,9 +1,16 @@
 import AsyncStorage from '@react-native-community/async-storage'
 const ASYNC_NAME = 'LOCAL_ARTICLES'
+const CATEGORY_ASYNC_NAME = 'CATEGORY_LOCAL_ARTICLES'
 
 const storetoAsync = (articles) => {
 	if(articles){
 		AsyncStorage.setItem(ASYNC_NAME, JSON.stringify(articles))
+	}
+}
+
+const storeCategoryArticlestoAsync = (articles) => {
+	if(articles){
+		AsyncStorage.setItem(CATEGORY_ASYNC_NAME, JSON.stringify(articles))
 	}
 }
 
@@ -22,8 +29,29 @@ const fetchfromAsync = () => {
 	})
 }
 
+const fetchCategoryArticlesfromAsync = () => {
+	return new Promise((resolve, reject) => {
+		AsyncStorage.getItem(CATEGORY_ASYNC_NAME)
+			.then((res) => {
+				if (res != null) {
+					res = JSON.parse(res)
+					resolve(res)
+				} else {
+					resolve([])
+				}
+			})
+			.catch((err) => reject(err))
+	})
+}
+
 const removeAsync = () => {
 	AsyncStorage.removeItem(ASYNC_NAME)
 }
 
-export { storetoAsync, fetchfromAsync, removeAsync }
+export { 
+	storetoAsync, 
+	storeCategoryArticlestoAsync,
+	fetchfromAsync, 
+	fetchCategoryArticlesfromAsync,
+	removeAsync,
+}
