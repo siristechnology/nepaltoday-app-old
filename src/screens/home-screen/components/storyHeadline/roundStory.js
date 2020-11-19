c87f87cba08f2b257e4f90744d2bd55260cd3d0f
Shiva Aryal <lostinhell99@gmail.com>
2020-11-19 21:28:39 +0545

    Nep 192 instagram like story (#199)
    * NEP-192 : Instagram like story done
    
    * NEP-192 : added short description in instagram story
    
    * NEP-192 : lgtm error fixed
    
    Co-authored-by: Shiva Aryal <shivaaryalj7@gmail.com>

---
+++ src/screens/home-screen/components/storyHeadline/roundStory.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { StyleSheet, Image, TouchableOpacity, Text } from 'react-native'
+
+const RoundStory = (props) => {
+    const article = props.article
+    return (
+        <TouchableOpacity
+            activeOpacity={0.8}
+            style={styles.storyView}
+            onPress={()=>props.onShowStory(article)}
+        >
+            <Image 
+                style={styles.storyImage}
+                source={{uri: article.imageLink}}
+            />
+            <Text style={styles.sourceText}>
+                {article.source.name}
+            </Text>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    storyImage: {
+        height: 70,
+        width: 70,
+        borderRadius: 40,
+        borderWidth: 3,
+        borderColor: '#e57373'
+    },
+    storyView: {
+        marginRight: 10,
+        alignItems: 'center'
+    },
+    sourceText: {
+        fontSize: 13,
+        color: '#000',
+        opacity: 0.7
+    }
+})
+
+export default RoundStory
---
+++ src/screens/home-screen/components/storyHeadline/swiperStory.js
@@ -0,0 +1,99 @@
+import React from 'react'
+import { StyleSheet, View, Text, Image } from 'react-native'
+import Swiper from 'react-native-swiper'
+import { Button } from 'react-native-ui-kitten/ui'
+import { IconButton } from 'react-native-paper'
+import { np } from '../../../../lang/np'
+
+const SwiperStory = (props) => {
+    const {article, articles} = props
+    const { READ_MORE } = np.public
+    const SingleStory = ({myArticle}) => {
+        return(
+            <View style={styles.container}>
+                <IconButton
+                    style={styles.closeButton}
+                    icon="close"
+                    size={25}
+                    color="#fff"
+                    onPress={props.onCloseStory}
+                />
+                <Image
+                    source={{uri: myArticle.imageLink}}
+                    style={styles.storyImage}
+                />
+                <Text style={styles.storyText}>
+                    {myArticle.title}
+                </Text>
+                <Text style={styles.shortDescription}>
+                    {myArticle.shortDescription ? myArticle.shortDescription.substring(0, 400) + '...' : ''}
+                </Text>
+                <Button style={styles.readMoreButton} onPress={()=>props.showArticleDetail(myArticle)}>
+					{READ_MORE}
+				</Button>
+            </View>
+        )  
+    }
+
+    return (
+        <Swiper 
+            showsButtons={true} 
+            autoplay={true}
+            loop={false}
+            showsPagination={false}
+            autoplayTimeout={5}
+            index={articles.indexOf(article)}
+        >
+            {articles.map((singleArticle,i)=>(
+                <SingleStory
+                    key={i}
+                    myArticle={singleArticle}
+                />
+            ))}
+        </Swiper>        
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0,0,0,0.95)'
+    },
+    storyImage: {
+        height: 250,
+        width: '100%',
+        resizeMode: 'stretch'
+    },
+    storyText: {
+        fontSize: 22,
+        marginTop: 40,
+        marginHorizontal: 20,
+        textAlign: 'center',
+        color: '#fff',
+        opacity: 0.9
+    },
+    shortDescription: {
+        fontSize: 16,
+        marginTop: 20,
+        marginHorizontal: 20,
+        textAlign: 'center',
+        color: '#fff',
+        opacity: 0.8
+    },
+    readMoreButton: {
+        marginTop: 70,
+        width: 200
+    },
+    closeButton: {
+        position: 'absolute',
+        right: 5,
+        top: 5,
+        zIndex: 111,
+        elevation: 1,
+        backgroundColor: '#000'
+    }
+})
+
+export default SwiperStory
