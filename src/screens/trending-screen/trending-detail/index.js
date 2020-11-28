e0c0a8d608431ac2462b01db5824535f5d6610bf
Shiva Aryal <lostinhell99@gmail.com>
2020-11-28 10:58:21 +0545

    removing samachar tab of trending person if no news is present (#212)
    Co-authored-by: Shiva Aryal <shivaaryalj7@gmail.com>

--- src/screens/trending-screen/trending-detail/index.js
+++ src/screens/trending-screen/trending-detail/index.js
@@ -1,12 +1,60 @@
 import React from 'react'
-import { View, Image, Text, StyleSheet } from 'react-native'
+import { View, Image, Text, StyleSheet, ActivityIndicator } from 'react-native'
 import Icon from 'react-native-vector-icons/AntDesign'
 import { Tab, Tabs } from 'native-base'
 import TrendingTweetDetail from './trendingTweetDetail'
 import TrendingNewsDetail from './trendingNewsDetail'
+import { useQuery } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
 
 const TrendingDetail = (props) => {
 	const trending = props.route.params.trending
+
+	const GET_INDIVIDUAL_ARTICLES = gql`
+        query trendingDetail{
+            getIndividualArticles(name: "${trending.nepaliName}"){
+                _id
+                title
+                shortDescription
+                content
+                link
+                imageLink
+                createdDate
+                modifiedDate
+                category
+                tags
+                totalWeight
+                source {
+                    name
+                    logoLink
+                }
+            }
+        }
+	`
+	
+	const {loading: loading1, data: data1, refetch: refetch1} = useQuery(GET_INDIVIDUAL_ARTICLES, {
+		variables: {}
+	})
+
+	const GET_TWEETS_BY_HANDLE = gql`
+        query trendingDetail{
+            getTweetByHandle(handle: "${trending.handle}"){
+                _id,
+                text,
+                tweetId,
+                handle,
+                publishedDate,
+                name,
+                createdAt,
+                profileImage,
+                description,
+            }
+        }
+	`
+	
+	const {loading: loading2, data: data2, refetch: refetch2} = useQuery(GET_TWEETS_BY_HANDLE, {
+		variables: {}
+	})
 	return (
 		<View style={{ flex: 1 }}>
 			<View style={styles.headerView}>
@@ -18,6 +66,11 @@ const TrendingDetail = (props) => {
 				<Image source={{ uri: trending.image }} style={styles.imageStyle} />
 				<Text style={{ marginTop: 10, fontSize: 14 }}>{trending.handle}</Text>
 			</View>
+			{(loading1 || loading2) &&
+				<View style={styles.loaderContainer}>
+					<ActivityIndicator size="large" color="#000" />
+				</View>
+			||
             <Tabs tabBarUnderlineStyle={{ backgroundColor: '#ff0000' }}>
 				<Tab
 					style={{ flex: 1 }}
@@ -28,23 +81,26 @@ const TrendingDetail = (props) => {
 					activeTextStyle={{ color: '#000' }}
 				>
 					<TrendingTweetDetail
-                        trending={trending}
+						data={data2}
+						refetch={refetch2}
                     />
 				</Tab>
-				<Tab
-					style={{ flex: 1 }}
-					heading="समाचार"
-					tabStyle={{ backgroundColor: '#fff' }}
-					activeTabStyle={{ backgroundColor: '#fff' }}
-					textStyle={{ color: '#000' }}
-					activeTextStyle={{ color: '#000' }}
-				>
-                    <TrendingNewsDetail
-						navigation={props.navigation}
-                        trending={trending}
-                    />
-				</Tab>
-			</Tabs>
+				{data1.getIndividualArticles.length>0 &&
+					<Tab
+						style={{ flex: 1 }}
+						heading="समाचार"
+						tabStyle={{ backgroundColor: '#fff' }}
+						activeTabStyle={{ backgroundColor: '#fff' }}
+						textStyle={{ color: '#000' }}
+						activeTextStyle={{ color: '#000' }}
+					>
+						<TrendingNewsDetail
+							navigation={props.navigation}
+							data={data1}
+							refetch={refetch1}
+						/>
+				</Tab>}
+			</Tabs>}
 		</View>
 	)
 }
@@ -72,7 +128,11 @@ const styles = StyleSheet.create({
 		height: 75,
 		width: 75,
 		borderRadius: 40,
-	}
+	},
+	loaderContainer: {
+		marginTop: 30,
+		justifyContent: 'center',
+	},
 })
 
 export default TrendingDetail
--- src/screens/trending-screen/trending-detail/trendingTweetDetail.js
+++ src/screens/trending-screen/trending-detail/trendingTweetDetail.js
@@ -1,31 +1,10 @@
 import React, { useState } from 'react'
-import { View, StyleSheet, ActivityIndicator, RefreshControl } from 'react-native'
-import gql from 'graphql-tag'
-import { useQuery } from '@apollo/react-hooks'
+import { View } from 'react-native'
 import { TwitterListContainer } from '../../../layout/twitter/twitter-list.container'
 
 const TrendingTweetDetail = (props) => {
 	const [refreshing, setRefreshing] = useState(false)
-
-	const GET_TWEETS_BY_HANDLE = gql`
-        query trendingDetail{
-            getTweetByHandle(handle: "${props.trending.handle}"){
-                _id,
-                text,
-                tweetId,
-                handle,
-                publishedDate,
-                name,
-                createdAt,
-                profileImage,
-                description,
-            }
-        }
-    `
-
-	const { loading, data, refetch } = useQuery(GET_TWEETS_BY_HANDLE, {
-		variables: {},
-	})
+	const {data, refetch} = props
 
 	const handleRefresh = () => {
 		setRefreshing(true)
@@ -34,38 +13,13 @@ const TrendingTweetDetail = (props) => {
 
 	return (
 		<View style={{ flex: 1 }}>
-			<View refreshControl={<RefreshControl refreshing={refreshing} onRefresh={handleRefresh} />}>
-				{(loading && (
-					<View style={styles.loaderContainer}>
-						<ActivityIndicator size="large" color="#000" />
-					</View>
-				)) || (
-					<TwitterListContainer
-						tweets={data.getTweetByHandle}
-					/>
-				)}
-			</View>
+			<TwitterListContainer
+				refreshing={refreshing} 
+				onRefresh={handleRefresh}
+				tweets={data.getTweetByHandle}
+			/>
 		</View>
 	)
 }
-
-const styles = StyleSheet.create({
-	headerView: {
-		padding: 10,
-		backgroundColor: '#fff',
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-	},
-	headerText: {
-		fontSize: 19,
-		color: '#000',
-		opacity: 0.8,
-		fontWeight: 'bold',
-	},
-	loaderContainer: {
-		marginTop: 30,
-		justifyContent: 'center',
-	},
-})
 
 export default TrendingTweetDetail
